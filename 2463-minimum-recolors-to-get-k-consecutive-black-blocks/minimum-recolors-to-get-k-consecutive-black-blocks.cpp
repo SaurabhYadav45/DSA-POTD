6d929a4403cc5fd6e3a7b806ae430ec2dfd7c02f
Saurabh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-08 12:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (62.68%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -4,16 +4,44 @@ public:
         int n = blocks.length();
 
         int minOperation = INT_MAX;
+        int count = 0;
 
-        for(int i=0; i<=n-k; i++){
-            int whiteCount = 0;
-            for(int j=i; j<i+k; j++){
-                if(blocks[j] == 'W'){
-                    whiteCount++;
-                }
+        int i=0; 
+        int j=0;
+        while(j < n){
+            if(blocks[j] == 'W'){
+                count++;
             }
-            minOperation = min(minOperation, whiteCount);
+            if(j-i+1 == k){
+                minOperation = min(minOperation, count);
+                if(blocks[i] == 'W'){
+                    count--;
+                }
+                i++;
+            }
+            j++;
         }
         return minOperation;
     }
 };
+
+
+// class Solution {
+// public:
+//     int minimumRecolors(string blocks, int k) {
+//         int n = blocks.length();
+
+//         int minOperation = INT_MAX;
+
+//         for(int i=0; i<=n-k; i++){
+//             int whiteCount = 0;
+//             for(int j=i; j<i+k; j++){
+//                 if(blocks[j] == 'W'){
+//                     whiteCount++;
+//                 }
+//             }
+//             minOperation = min(minOperation, whiteCount);
+//         }
+//         return minOperation;
+//     }
+// };

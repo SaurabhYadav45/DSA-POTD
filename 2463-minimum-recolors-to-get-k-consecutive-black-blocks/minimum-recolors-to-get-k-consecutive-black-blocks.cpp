f6ced2d565b922d1fd5f9b2b0a9b647b322dc144
Saurabh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-08 12:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (31.01%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -3,27 +3,59 @@ public:
     int minimumRecolors(string blocks, int k) {
         int n = blocks.length();
 
-        int minOperation = INT_MAX;
-        int count = 0;
+        queue<char>q;
 
-        int i=0; 
-        int j=0;
-        while(j < n){
-            if(blocks[j] == 'W'){
-                count++;
+        int whiteCount = 0;
+        while(q.size() < k){
+            char curr = blocks[q.size()];
+            if(curr == 'W'){
+                whiteCount++;
             }
-            if(j-i+1 == k){
-                minOperation = min(minOperation, count);
-                if(blocks[i] == 'W'){
-                    count--;
-                }
-                i++;
-            }
-            j++;
+            q.push(curr);
+        }
+
+        int minOperation = whiteCount;
+
+        for(int i=k; i<n; i++){
+            if(q.front() == 'W')whiteCount--;
+            q.pop();
+
+            if(blocks[i] == 'W') whiteCount++;
+            q.push(blocks[i]);
+
+            minOperation = min(minOperation, whiteCount);
         }
         return minOperation;
     }
 };
+
+
+// class Solution {
+// public:
+//     int minimumRecolors(string blocks, int k) {
+//         int n = blocks.length();
+
+//         int minOperation = INT_MAX;
+//         int count = 0;
+
+//         int i=0; 
+//         int j=0;
+//         while(j < n){
+//             if(blocks[j] == 'W'){
+//                 count++;
+//             }
+//             if(j-i+1 == k){
+//                 minOperation = min(minOperation, count);
+//                 if(blocks[i] == 'W'){
+//                     count--;
+//                 }
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return minOperation;
+//     }
+// };
 
 
 // class Solution {

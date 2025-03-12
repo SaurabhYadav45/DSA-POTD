2b68d167301fb9a9df9e150ab97e422b383d6ef0
Saurabh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-12 20:06:45 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (43.55%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int posCount = 0;
-        int negCount = 0;
-
-        for(int& num : nums){
-            if(num > 0) posCount++;
-            else if(num < 0) negCount++;
-        }
+        int n = nums.size();
+        int posCount = nums.end() - upper_bound(nums.begin(), nums.end(), 0);
+        int negCount = lower_bound(nums.begin(), nums.end(), 0) - nums.begin();
         return max(posCount, negCount);
     }
 };
+
+
+//                  ********* BRute Force **********
+//                  ********* T.C = O (n)
+
+// class Solution {
+// public:
+//     int maximumCount(vector<int>& nums) {
+//         int posCount = 0;
+//         int negCount = 0;
+
+//         for(int& num : nums){
+//             if(num > 0) posCount++;
+//             else if(num < 0) negCount++;
+//         }
+//         return max(posCount, negCount);
+//     }
+// };

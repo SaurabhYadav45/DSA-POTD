4bc9433cca25ce837410d967311259352a7960a0
Saurabh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-19 23:26:55 +0530

    Time: 7 ms (59.94%) | Memory: 140.1 MB (47.55%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,24 +1,47 @@
-
-    class Solution {
+class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        deque<int> flipQueue;
         int count = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            while (!flipQueue.empty() && i > flipQueue.front() + 2) {
-                flipQueue.pop_front();
-            }
-
-            if ((nums[i] + flipQueue.size()) % 2 == 0) {
-                if (i + 2 >= nums.size()) {
-                    return -1;
-                }
+        for (int i = 2; i < nums.size(); i++) {
+            // only looking forward to the last element
+            if (nums[i - 2] == 0) {
                 count++;
-                flipQueue.push_back(i);
+                // flip i-2 and i-1 and i
+                nums[i - 2] ^= 1;
+                nums[i - 1] ^= 1;
+                nums[i] ^= 1;
             }
         }
-
-        return count;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum == nums.size()) return count;
+        return -1;
     }
 };
+
+
+
+
+
+//     class Solution {
+// public:
+//     int minOperations(vector<int>& nums) {
+//         deque<int> flipQueue;
+//         int count = 0;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             while (!flipQueue.empty() && i > flipQueue.front() + 2) {
+//                 flipQueue.pop_front();
+//             }
+
+//             if ((nums[i] + flipQueue.size()) % 2 == 0) {
+//                 if (i + 2 >= nums.size()) {
+//                     return -1;
+//                 }
+//                 count++;
+//                 flipQueue.push_back(i);
+//             }
+//         }
+
+//         return count;
+//     }
+// };

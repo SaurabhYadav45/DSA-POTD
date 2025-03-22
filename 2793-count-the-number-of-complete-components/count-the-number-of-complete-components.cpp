7206c30d1190e8bf5fb8b8072ed33cd98d7e8b0f
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-22 23:27:04 +0530

    Time: 69 ms (38.06%) | Memory: 141 MB (22.72%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    void dfs(int i, unordered_map<int, vector<int>>&adj, vector<bool>&visited, int& v, int& e){
+    void bfs(int i, unordered_map<int, vector<int>>&adj, vector<bool>&visited, int& v, int& e){
+        queue<int>q;
+        q.push(i);
         visited[i] = true;
-        v++;
-        e += adj[i].size();
 
-        for(auto& nbr : adj[i]){
-            if(!visited[nbr]){
-                dfs(nbr, adj, visited, v, e);
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            v++;
+            e += adj[curr].size();
+
+            for(int& nbr : adj[curr]){
+                if(!visited[nbr]){
+                    q.push(nbr);
+                    visited[nbr] = true;
+                }
             }
         }
     }
@@ -29,7 +37,7 @@ public:
 
             int v = 0;
             int e = 0;
-            dfs(i, adj, visited, v, e);
+            bfs(i, adj, visited, v, e);
 
             if((v*(v-1)/2) == e/2){
                 count++;
@@ -38,3 +46,45 @@ public:
         return count;
     }
 };
+
+
+// class Solution {
+// public:
+//     void dfs(int i, unordered_map<int, vector<int>>&adj, vector<bool>&visited, int& v, int& e){
+//         visited[i] = true;
+//         v++;
+//         e += adj[i].size();
+
+//         for(auto& nbr : adj[i]){
+//             if(!visited[nbr]){
+//                 dfs(nbr, adj, visited, v, e);
+//             }
+//         }
+//     }
+//     int countCompleteComponents(int n, vector<vector<int>>& edges) {
+//         unordered_map<int, vector<int>>adj;
+
+//         for(auto& edge : edges){
+//             int u = edge[0];
+//             int v = edge[1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+
+//         vector<bool>visited(n, false);
+//         int count = 0;
+        
+//         for(int i=0; i<n; i++){
+//             if(visited[i] == true) continue;
+
+//             int v = 0;
+//             int e = 0;
+//             dfs(i, adj, visited, v, e);
+
+//             if((v*(v-1)/2) == e/2){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };

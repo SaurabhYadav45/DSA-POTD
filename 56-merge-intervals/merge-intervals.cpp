d2143c9907de65fe82a7087a8757ff0624ef83d6
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-25 21:55:46 +0530

    Time: 7 ms (44.16%) | Memory: 24 MB (27.79%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -8,11 +8,11 @@ public:
         result.push_back(intervals[0]);
 
         for(int i=1; i<n; i++){
-            if(intervals[i][0] > result.back()[1]){
-                result.push_back(intervals[i]);
-            }
-            else if(intervals[i][0] <= result.back()[1]){
+            if(intervals[i][0] <= result.back()[1]){
                 result.back()[1] = max(result.back()[1], intervals[i][1]);
+            }
+            else{
+                result.push_back(intervals[i]);
             }
         }
         return result;

138c7d3ac55f0c65edb5161bdddf22b0daf5647b
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-25 21:55:01 +0530

    Time: 3 ms (89.75%) | Memory: 23.8 MB (80.15%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,23 +4,46 @@ public:
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
 
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-
         vector<vector<int>>result;
+        result.push_back(intervals[0]);
 
         for(int i=1; i<n; i++){
-            if(intervals[i][0] > end){
-                result.push_back({start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+            if(intervals[i][0] > result.back()[1]){
+                result.push_back(intervals[i]);
             }
-            else if(intervals[i][0] <= end){
-                start = min(start, intervals[i][0]);
-                end  = max(end, intervals[i][1]);
+            else if(intervals[i][0] <= result.back()[1]){
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
             }
         }
-        result.push_back({start, end});
         return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         int n = intervals.size();
+//         sort(intervals.begin(), intervals.end());
+
+//         int start = intervals[0][0];
+//         int end = intervals[0][1];
+
+//         vector<vector<int>>result;
+
+//         for(int i=1; i<n; i++){
+//             if(intervals[i][0] > end){
+//                 result.push_back({start, end});
+//                 start = intervals[i][0];
+//                 end = intervals[i][1];
+//             }
+//             else if(intervals[i][0] <= end){
+//                 start = min(start, intervals[i][0]);
+//                 end  = max(end, intervals[i][1]);
+//             }
+//         }
+//         result.push_back({start, end});
+//         return result;
+//     }
+// };

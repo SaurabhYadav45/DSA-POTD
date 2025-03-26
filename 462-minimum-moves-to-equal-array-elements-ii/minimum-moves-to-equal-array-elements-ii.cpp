af6d6045ae59fcf0d20f55a837b0abe1d6ef04ee
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-26 21:15:46 +0530

    Time: 3 ms (30.04%) | Memory: 15.8 MB (50.27%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,7 +3,10 @@ public:
     int minMoves2(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+
+        // partial sorting
+        nth_element(nums.begin(), nums.begin()+ n/2, nums.end());
 
         int median = nums[n/2];
 

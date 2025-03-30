beda5cb8200f6f7c8f680815ab27345a69cbcf29
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-03-30 21:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (71.56%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,43 +1,70 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<vector<int>>intervals;
-        unordered_map<char, pair<int, int>>mp;
+        unordered_map<char, int>lastOccurence;
 
         for(int i=0; i<s.length(); i++){
-            if(mp.find(s[i]) == mp.end()){
-                mp[s[i]] = {i, i};
-            }
-            else{
-                mp[s[i]].second = i;
-            }
+            lastOccurence[s[i]] = i;
         }
 
-        for(auto& it: mp){
-            pair<int, int>p = it.second;
-            int start = p.first;
-            int end = p.second;
-            intervals.push_back({start, end});
-        }
+        vector<int>result;
+        int start = 0;
+        int end = 0;
 
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>>result;
-        result.push_back(intervals[0]);
-
-        for(int i=1; i<intervals.size(); i++){
-            if(intervals[i][0] < result.back()[1]){
-                result.back()[1] = max(result.back()[1], intervals[i][1]);
-            }
-            else{
-                result.push_back(intervals[i]);
+        for(int i=0; i<s.length(); i++){
+            end = max(end,  lastOccurence[s[i]]);
+            if(i == end){
+                result.push_back(end-start+1);
+                start  = i + 1;
             }
         }
-
-        vector<int>res(result.size(), 0);
-
-        for(int i=0; i<result.size(); i++){
-            res[i] = result[i][1]-result[i][0]+1;
-        }
-        return res;
+        return result;
     }
 };
+
+
+//                            *********  Using Merge Interval *******
+
+// class Solution {
+// public:
+//     vector<int> partitionLabels(string s) {
+//         vector<vector<int>>intervals;
+//         unordered_map<char, pair<int, int>>mp;
+
+//         for(int i=0; i<s.length(); i++){
+//             if(mp.find(s[i]) == mp.end()){
+//                 mp[s[i]] = {i, i};
+//             }
+//             else{
+//                 mp[s[i]].second = i;
+//             }
+//         }
+
+//         for(auto& it: mp){
+//             pair<int, int>p = it.second;
+//             int start = p.first;
+//             int end = p.second;
+//             intervals.push_back({start, end});
+//         }
+
+//         sort(intervals.begin(), intervals.end());
+//         vector<vector<int>>result;
+//         result.push_back(intervals[0]);
+
+//         for(int i=1; i<intervals.size(); i++){
+//             if(intervals[i][0] < result.back()[1]){
+//                 result.back()[1] = max(result.back()[1], intervals[i][1]);
+//             }
+//             else{
+//                 result.push_back(intervals[i]);
+//             }
+//         }
+
+//         vector<int>res(result.size(), 0);
+
+//         for(int i=0; i<result.size(); i++){
+//             res[i] = result[i][1]-result[i][0]+1;
+//         }
+//         return res;
+//     }
+// };

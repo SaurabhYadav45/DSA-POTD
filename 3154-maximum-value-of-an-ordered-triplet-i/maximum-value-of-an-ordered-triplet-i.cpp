bc68b874afacff5f88fe56b4c4bd5be6f179e7b9
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-04-02 18:39:31 +0530

    Time: 1 ms (42.77%) | Memory: 22.6 MB (17.47%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-
         long long result = 0;
 
-        int maxPrefix = nums[0];
-        
-        for(int j=1; j<n; j++){
-            for(int k= j+1; k<n; k++){
-                result = max(result, (long long)(maxPrefix-nums[j])*nums[k]);
-                maxPrefix = max(maxPrefix, nums[j]);
-            }
+        vector<int>prefix(n), suffix(n);
+
+        for(int i=1; i<n; i++){
+            prefix[i] = max(prefix[i-1], nums[i-1]);
+            suffix[n-i-1] = max(suffix[n-i], nums[n-i]);
         }
+
+        for(int i=1; i<n-1; i++){
+            result = max(result, (long long)(prefix[i] - nums[i])*suffix[i]);
+        }
+        
         return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     long long maximumTripletValue(vector<int>& nums) {
+//         int n = nums.size();
+
+//         long long result = 0;
+
+//         int maxPrefix = nums[0];
+        
+//         for(int j=1; j<n; j++){
+//             for(int k= j+1; k<n; k++){
+//                 result = max(result, (long long)(maxPrefix-nums[j])*nums[k]);
+//                 maxPrefix = max(maxPrefix, nums[j]);
+//             }
+//         }
+//         return result;
+//     }
+// };
 
 
 // class Solution {

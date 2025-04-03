0ddd5fc134d61baad02fdac99ae78d20b94e1e9a
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-04-03 19:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 91.1 MB (85.24%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -3,20 +3,43 @@ public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
 
-        vector<long long>prefix(n), suffix(n);
+        long long highest = 0;
+        long long highestDiff = 0;
+        long long result = 0;
 
-        for(int i=1; i<n; i++){
-            prefix[i] = max(prefix[i-1], (long long)nums[i-1]);
-            suffix[n-i-1] = max(suffix[n-i], (long long)nums[n-i]);
+        for(long long num : nums){
+            result = max(result, highestDiff*num);
+            highestDiff = max(highestDiff, highest-num);
+            highest = max(highest, num);
         }
-
-        long long maxSum = INT_MIN;
-
-        for(int i=1; i<n-1; i++){
-            maxSum = max(maxSum, (long long)(prefix[i]-nums[i])*suffix[i]);
-        }
-        long long result = maxSum >0 ? maxSum : 0;
-        return result;
+        return result >0 ? result : 0;
     }
-
 };
+
+
+
+
+//                          Using Prefix Sum
+
+// class Solution {
+// public:
+//     long long maximumTripletValue(vector<int>& nums) {
+//         int n = nums.size();
+
+//         vector<long long>prefix(n), suffix(n);
+
+//         for(int i=1; i<n; i++){
+//             prefix[i] = max(prefix[i-1], (long long)nums[i-1]);
+//             suffix[n-i-1] = max(suffix[n-i], (long long)nums[n-i]);
+//         }
+
+//         long long maxSum = INT_MIN;
+
+//         for(int i=1; i<n-1; i++){
+//             maxSum = max(maxSum, (long long)(prefix[i]-nums[i])*suffix[i]);
+//         }
+//         long long result = maxSum >0 ? maxSum : 0;
+//         return result;
+//     }
+
+// };

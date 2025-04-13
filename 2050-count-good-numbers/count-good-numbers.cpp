b624ff2051be4f4ee42e1171d3e9d6709f96b0aa
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-04-13 16:47:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (34.93%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    const int MOD = 1e9+7;
+    long long calculatePower(long long a, long long b){
+        if(b == 0) return 1;
+        long long half = calculatePower(a, b/2);
+        long long result = (half*half) % MOD;
+        if(b % 2 == 1){
+            result = (result*a)%MOD;
+        }
+        return result;
+    }
+    int countGoodNumbers(long long n) {
+        long long evenCount = (n+1)/2;
+        long long oddCount = n/2;
+
+        long long result = (calculatePower(5, evenCount)* calculatePower(4, oddCount)) % MOD;
+        return result;
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     const int MOD = 1e9+7;
+//     bool checkGoodNumbers(long long num){
+//         string s = to_string(num);
+//         for(int i=0; i<s.length(); i++){
+//             int n = s[i]+'0';
+//             if(i%2 != n%2){
+//                 return false;
+//             }
+//         }
+//         return false;
+//     }
+//     int countGoodNumbers(long long n) {
+//         long long start = pow(10, n-1);
+//         long long end = pow(10, n)-1;
+
+//         long long count = 0;
+//         for(long long num = start; num<=end; num++){
+//             if(checkGoodNumbers(num)){
+//                 count++;
+//             }
+//         }
+//         return count%MOD;
+//     }
+// };

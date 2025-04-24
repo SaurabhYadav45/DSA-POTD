561aab4d36b3c300319179b416321fc98d70aec8
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-04-25 00:19:47 +0530

    Time: 24 ms (45.78%) | Memory: 41.9 MB (59.44%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,21 +1,53 @@
+
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
         int n  = nums.size();
         unordered_set<int>st{nums.begin(), nums.end()};
         int distinct_size = st.size();
-
         int count = 0;
+        unordered_map<int, int>mp;
 
-        for(int i=0; i<n; i++){
-            unordered_set<int>temp;
-            for(int j=i; j<n; j++){
-                temp.insert(nums[j]);
-                if(temp.size() == distinct_size){
-                    count++;
+        int i=0; 
+        int j=0;
+        while(j < n){
+            mp[nums[j]]++;
+            while(mp.size() == distinct_size){
+                count += n - j;
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0){
+                    mp.erase(nums[i]);
                 }
+                i++;
             }
+            
+            j++;
         }
+
         return count;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int countCompleteSubarrays(vector<int>& nums) {
+//         int n  = nums.size();
+//         unordered_set<int>st{nums.begin(), nums.end()};
+//         int distinct_size = st.size();
+
+//         int count = 0;
+
+//         for(int i=0; i<n; i++){
+//             unordered_set<int>temp;
+//             for(int j=i; j<n; j++){
+//                 temp.insert(nums[j]);
+//                 if(temp.size() == distinct_size){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };

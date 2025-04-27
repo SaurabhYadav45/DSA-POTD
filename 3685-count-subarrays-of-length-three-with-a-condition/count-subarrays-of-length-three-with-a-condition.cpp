fecac20a73aeee2ab083290398d06752ed55b273
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-04-27 12:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (75.30%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
@@ -5,17 +5,11 @@ public:
         int count = 0;
 
         for(int i=0; i<=n-3; i++){
-            vector<int>temp;
-            for(int j=i; j<i+3; j++){
-                temp.push_back(nums[j]);
-                if(temp.size() == 3){
-                    int first = temp[0];
-                    int second = temp[1];
-                    int third = temp[2];
+            int first = nums[i];
+            int second = nums[i+1];
+            int third = nums[i+2];
 
-                    if(first + third  == ((double)second/2))count++;
-                }
-            }
+            if(first + third == (double)second/2)count++;
         }
         return count;
     }

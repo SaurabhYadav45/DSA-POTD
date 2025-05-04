1f771fc4c4161c084dfe7d9ce0243ee00ca81b5b
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-05-04 19:52:21 +0530

    Time: 3 ms (58.50%) | Memory: 26.2 MB (48.50%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -2,19 +2,44 @@ class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
         int n = dominoes.size();
-        int count = 0;
+        int result = 0;
+        unordered_map<string, int>mp;
 
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int a = dominoes[i][0];
-                int b = dominoes[i][1];
-                int c = dominoes[j][0];
-                int d = dominoes[j][1];
-                if(a == c && b == d  || a == d && b == c){
-                    count++;
-                }
-            }
+        for(auto& vec: dominoes){
+            string s = "";
+            s.push_back(vec[0]);
+            s.push_back(vec[1]);
+            sort(s.begin(), s.end());
+            mp[s]++;
         }
-        return count;
+
+        for(auto& it : mp){
+            int count  = it.second;
+            result += (count*(count-1))/2;
+        }
+        return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+//         int n = dominoes.size();
+//         int count = 0;
+
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 int a = dominoes[i][0];
+//                 int b = dominoes[i][1];
+//                 int c = dominoes[j][0];
+//                 int d = dominoes[j][1];
+//                 if(a == c && b == d  || a == d && b == c){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };

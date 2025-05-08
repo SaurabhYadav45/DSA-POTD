914dd760e03b036bf73d193a0cc7604fea397fbe
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-05-08 16:22:15 +0530

    Time: 16 ms (85.70%) | Memory: 29.5 MB (58.49%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -1,46 +1,42 @@
-class State {
-public:
-    int x;
-    int y;
-    int dis;
-    State(int x, int y, int dis) : x(x), y(y), dis(dis) {}
-};
-
 class Solution {
 public:
+    typedef pair<int, pair<int, int>> P;
+    vector<vector<int>>directions{{0,1}, {0,-1}, {1,0}, {-1, 0}};
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int inf = 0x3f3f3f3f;
-        int n = moveTime.size(), m = moveTime[0].size();
-        vector<vector<int>> d(n, vector<int>(m, inf));
-        vector<vector<int>> v(n, vector<int>(m, 0));
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        vector<vector<int>>result(n, vector<int>(m, INT_MAX));
+        priority_queue<P, vector<P>, greater<P>>pq;
+        
+        result[0][0] = 0;
+        pq.push({0, {0,0}});
 
-        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        d[0][0] = 0;
+        while(!pq.empty()){
+            int currTime = pq.top().first;
+            auto cell = pq.top().second ;
+            int x = cell.first;
+            int y = cell.second;
+            pq.pop();
 
-        auto cmp = [](const State& a, const State& b) { return a.dis > b.dis; };
+            if(x == n-1 && y == m-1){
+                return currTime;
+            }
 
-        priority_queue<State, vector<State>, decltype(cmp)> q(cmp);
-        q.push(State(0, 0, 0));
+            for(auto& dir : directions){
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
 
-        while (!q.empty()) {
-            State s = q.top();
-            q.pop();
-            if (v[s.x][s.y]) continue;
-            v[s.x][s.y] = 1;
+                if(x_ >= 0 && x_ < n && y_ >= 0 && y_ < m){
+                    int waitTime = max(moveTime[x_][y_] - currTime, 0);
+                    int totalTime = waitTime + currTime + 1;
 
-            for (int i = 0; i < 4; i++) {
-                int nx = s.x + dirs[i][0];
-                int ny = s.y + dirs[i][1];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-
-                int dist = max(d[s.x][s.y], moveTime[nx][ny]) + 1;
-                if (d[nx][ny] > dist) {
-                    d[nx][ny] = dist;
-                    q.push(State(nx, ny, dist));
+                    if(result[x_][y_] > totalTime){
+                        result[x_][y_] = totalTime;
+                        pq.push({totalTime, {x_,y_}});
+                    }
                 }
             }
         }
-
-        return d[n - 1][m - 1];
+        return result[n-1][m-1];
     }
 };

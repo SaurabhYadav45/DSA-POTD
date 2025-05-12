19169943bcd2e0e7ca318b9d714f6506b5d6dc76
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-05-12 12:44:02 +0530

    Time: 51 ms (35.84%) | Memory: 32.3 MB (5.72%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> result;
+        unordered_map<int, int> freq;
+
+        // Count frequency of each digit
+        for (int d : digits) {
+            freq[d]++;
+        }
+
+        // Iterate all 3-digit even numbers
+        for (int num = 100; num <= 999; num += 2) {
+            int a = num / 100;
+            int b = (num / 10) % 10;
+            int c = num % 10;
+
+            unordered_map<int, int> temp = freq;
+            if (--temp[a] >= 0 && --temp[b] >= 0 && --temp[c] >= 0) {
+                result.push_back(num);
+            }
+        }
+
+        sort(result.begin(), result.end());
+        return result;
+    }
+};

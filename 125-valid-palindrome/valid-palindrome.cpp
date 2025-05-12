4f66f16c67534c38550a5bd0ceb5642fef225698
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-05-12 23:10:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (5.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+    bool checkPalindrome(string s){
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
     bool isPalindrome(string s) {
         string ans = "";
-        // unordered_set<char>st;
-
-        // for(char ch='a'; ch <='z'; ch++){
-        //     st.insert(ch);
-        // }
-
         
         for(int i =0; i<s.size(); i++){
             char ch = s[i];
@@ -16,11 +23,15 @@ public:
             }
         }
 
-        string s2 = ans;
-        reverse(s2.begin(), s2.end());
-        if(ans == s2){
+        if(checkPalindrome(ans)){
             return true;
         }
         return false;
+        // string s2 = ans;
+        // reverse(s2.begin(), s2.end());
+        // if(ans == s2){
+        //     return true;
+        // }
+        // return false;
     }
 };

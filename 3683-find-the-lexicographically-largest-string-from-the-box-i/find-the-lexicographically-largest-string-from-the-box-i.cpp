52ac2eb7ea43cbac5f062da3f9f4873f647a6355
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-05 21:22:54 +0530

    Time: 7 ms (91.92%) | Memory: 10.5 MB (89.56%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,14 +1,51 @@
+// class Solution {
+// public:
+//     string answerString(string word, int numFriends) {
+//         if (numFriends == 1) {
+//             return word;
+//         }
+//         int n = word.size();
+//         string res;
+//         for (int i = 0; i < n; i++) {
+//             res = max(res, word.substr(i, min(n - numFriends + 1, n - i)));
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
     string answerString(string word, int numFriends) {
-        if (numFriends == 1) {
+
+        if(numFriends==1){
             return word;
         }
-        int n = word.size();
-        string res;
-        for (int i = 0; i < n; i++) {
-            res = max(res, word.substr(i, min(n - numFriends + 1, n - i)));
-        }
-        return res;
+        
+       int n=word.length();
+       int maxi=n-numFriends+1;
+       vector<string>v;
+
+       string s=word.substr(0,maxi-1);
+       string  ans=s;
+       for(int i=0;i<=n-maxi;i++){
+            s+=word[maxi+i-1];
+            // v.push_back(s);
+            if(s>ans)ans =s;
+            s.erase(0,1);
+       }
+       
+    
+       while(s.size()){
+            if(s>ans)ans =s;
+        //   v.push_back(s);
+          s.erase(0,1);
+
+       }
+
+        // sort(v.begin(),v.end());
+
+        return ans;
+
     }
 };

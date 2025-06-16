1f125124aec34075117da1e5bddfc7eb4c1b902a
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-16 09:56:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (5.67%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -1,14 +1,37 @@
+// class Solution {
+// public:
+//     int maximumDifference(vector<int>& nums) {
+//         int n = nums.size();
+//         int result = -1;
+
+//         for(int i=0; i<n; i++){
+//             for(int j=i; j<n; j++){
+//                 if(nums[i] < nums[j]){
+//                     result = max(result, nums[j]-nums[i]);
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         int n = nums.size();
         int result = -1;
 
+        vector<int>pSum(n, 0);
+        pSum[n-1] = nums[n-1];
+
+        for(int i=n-2; i>=0; i--){
+            pSum[i] = max(nums[i], pSum[i+1]);
+        }
+        
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(nums[i] < nums[j]){
-                    result = max(result, nums[j]-nums[i]);
-                }
+            if(nums[i] < pSum[i]){
+                result = max(result, pSum[i] - nums[i]);
             }
         }
         return result;

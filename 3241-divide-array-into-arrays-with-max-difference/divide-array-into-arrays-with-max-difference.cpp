7923efc8af0560dacd35411530e1721b5e2fc89d
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-18 19:44:31 +0530

    Time: 46 ms (70.57%) | Memory: 120.1 MB (44.50%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,3 +1,27 @@
+// class Solution {
+// public:
+//     vector<vector<int>> divideArray(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+
+//         vector<vector<int>>result;
+//         vector<int>vec;
+
+//         for(int i=0; i<n; i++){
+//             if(!vec.empty() && abs(nums[i] - vec.front()) > k){
+//                 return {};
+//             }
+//             vec.push_back(nums[i]);
+//             if(vec.size() == 3){
+//                 result.push_back(vec);
+//                 vec.clear();   // T.C = O(n)
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
@@ -5,16 +29,15 @@ public:
         sort(nums.begin(), nums.end());
 
         vector<vector<int>>result;
-        vector<int>vec;
+        vector<int>vec(3);
 
         for(int i=0; i<n; i++){
-            if(!vec.empty() && abs(nums[i] - vec.front()) > k){
+            if((i % 3 > 0) && abs(nums[i] - vec.front()) > k){
                 return {};
             }
-            vec.push_back(nums[i]);
-            if(vec.size() == 3){
+            vec[i % 3] = nums[i];
+            if(i % 3 == 2){
                 result.push_back(vec);
-                vec.clear();
             }
         }
         return result;

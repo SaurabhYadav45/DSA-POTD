b4b2cb4340a3a16de77df827782f489b34d9d1bd
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-18 19:48:58 +0530

    Time: 47 ms (66.75%) | Memory: 120 MB (81.34%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,3 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>>result;
+
+        for(int i=0; i<n-2; i = i+3){
+            if(nums[i+2] - nums[i] <= k){
+                result.push_back({nums[i], nums[i+1], nums[i+2]});
+            }
+            else{
+                return {};
+            }
+        }
+        return result;
+    }
+};
+
+
+
 // class Solution {
 // public:
 //     vector<vector<int>> divideArray(vector<int>& nums, int k) {
@@ -22,24 +44,24 @@
 // };
 
 
-class Solution {
-public:
-    vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+// class Solution {
+// public:
+//     vector<vector<int>> divideArray(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
 
-        vector<vector<int>>result;
-        vector<int>vec(3);
+//         vector<vector<int>>result;
+//         vector<int>vec(3);
 
-        for(int i=0; i<n; i++){
-            if((i % 3 > 0) && abs(nums[i] - vec.front()) > k){
-                return {};
-            }
-            vec[i % 3] = nums[i];
-            if(i % 3 == 2){
-                result.push_back(vec);
-            }
-        }
-        return result;
-    }
-};
+//         for(int i=0; i<n; i++){
+//             if((i % 3 > 0) && abs(nums[i] - vec.front()) > k){
+//                 return {};
+//             }
+//             vec[i % 3] = nums[i];
+//             if(i % 3 == 2){
+//                 result.push_back(vec);
+//             }
+//         }
+//         return result;
+//     }
+// };

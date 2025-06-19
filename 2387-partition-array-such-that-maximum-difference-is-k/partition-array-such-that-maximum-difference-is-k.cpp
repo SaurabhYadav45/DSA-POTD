8a0e5518b4ffe38672d31b1522c5eb62a3f9c0d5
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-19 19:45:02 +0530

    Time: 39 ms (32.78%) | Memory: 87 MB (10.93%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -1,3 +1,25 @@
+// class Solution {
+// public:
+//     int partitionArray(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int count = 0;
+//         sort(nums.begin(), nums.end());
+        
+//         int i=0;
+//         while(i < n){
+//             int min = nums[i];
+//             int j = i+1;
+//             while(j < n && nums[j]-min <= k){
+//                 j++;
+//             }
+//             count++;
+//             i = j;
+//         }
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
@@ -5,16 +27,14 @@ public:
         int count = 0;
         sort(nums.begin(), nums.end());
         
-        int i=0;
-        while(i < n){
-            int min = nums[i];
-            int j = i+1;
-            while(j < n && nums[j]-min <= k){
-                j++;
+        int minVal = nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i]-minVal > k){
+                count++;
+                minVal = nums[i];
             }
-            count++;
-            i = j;
         }
+        count++;
         return count;
     }
 };

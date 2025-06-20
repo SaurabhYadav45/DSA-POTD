84d061aca95887e31e93f0ed8da4dc942f32c6bf
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-20 16:35:02 +0530

    Time: 3 ms (32.63%) | Memory: 15.6 MB (16.05%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,71 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>>result;
+//         for(int i=0; i<(1<<n); i++){
+//             vector<int>subset;
+//             for(int j=0; j<n; j++){
+//                 if((1<<j) & i){
+//                     subset.push_back(nums[j]);
+//                 }
+//             }
+//             result.push_back(subset);
+//         }
+//         return result;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    void solve(int i, vector<int>&nums, vector<int>curr, vector<vector<int>>&result){
+        if(i >= nums.size()){
+            result.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[i]);
+        solve(i+1, nums, curr, result);
+        curr.pop_back();
+
+        solve(i+1, nums, curr, result);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>result;
+        vector<int>temp;
+        solve(0, nums, {}, result);
+        return result;
+    }
+};
+
+
+
+// class Solution {
+// public:
+
+//     //  T.C = 2^n
+//     // S.C = O(n)
+//     // Stacl space = O(n)
+
+//     void subsetFinder(vector<int>& nums, vector<vector<int>>&ans, vector<int>&path , int i){
+//         // Base Case
+//         if(i >= nums.size()){
+//             ans.push_back(path);
+//             return;
+//         }
+//         // Exclude
+//         subsetFinder(nums, ans, path, i+1);
+//         // Include
+//         path.push_back(nums[i]);
+//         subsetFinder(nums, ans, path, i+1);
+//         path.pop_back();
+//     }
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>path;
+//         subsetFinder(nums, ans, path, 0);
+//         return ans;
+//     }
+// };

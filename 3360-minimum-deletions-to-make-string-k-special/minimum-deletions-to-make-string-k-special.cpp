dfcf678347101ef36e69d8b30d5439ef59bd957f
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-21 19:42:28 +0530

    Time: 23 ms (24.28%) | Memory: 20 MB (11.23%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -1,22 +1,56 @@
+// class Solution {
+// public:
+//     int minimumDeletions(string word, int k) {
+//         unordered_map<char, int>mp;
+//         unordered_set<char>st;
+//         int result = INT_MAX;
+
+//         for(char&ch: word){
+//             mp[ch]++;
+//             st.insert(ch);
+//         }
+
+//         for(auto& ch :st){
+//             int freq_i = mp[ch];
+//             int delete_freq = 0;
+//             for(auto& ch2 : st){
+//                 if(ch == ch2)continue;
+
+//                 int freq_j = mp[ch2];
+//                 if(freq_j > freq_i + k){
+//                     delete_freq += freq_j - freq_i - k;
+//                 }
+//                 else if(freq_j <  freq_i){
+//                     delete_freq += freq_j;
+//                 }
+//             }
+//             result  = min(result, delete_freq);
+//         }
+//         return result; 
+//     }
+// };
+
+
+
 class Solution {
 public:
     int minimumDeletions(string word, int k) {
-        unordered_map<char, int>mp;
+        vector<int>freq(26, 0);
         unordered_set<char>st;
         int result = INT_MAX;
 
         for(char&ch: word){
-            mp[ch]++;
+            freq[ch-'a']++;
             st.insert(ch);
         }
 
         for(auto& ch :st){
-            int freq_i = mp[ch];
+            int freq_i = freq[ch-'a'];
             int delete_freq = 0;
             for(auto& ch2 : st){
                 if(ch == ch2)continue;
 
-                int freq_j = mp[ch2];
+                int freq_j = freq[ch2-'a'];
                 if(freq_j > freq_i + k){
                     delete_freq += freq_j - freq_i - k;
                 }

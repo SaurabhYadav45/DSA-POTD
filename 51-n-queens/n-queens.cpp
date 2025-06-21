c16914e7fda123eb9a1ee44ec15585526c583232
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-21 09:46:05 +0530

    Time: 11 ms (19.04%) | Memory: 12.3 MB (21.69%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,31 +1,67 @@
+// class Solution {
+// public:
+//     vector<vector<string>>result;
+//     int N ;
+
+//     bool isValid(vector<string>&board, int row, int col){
+//         // upward
+//         for(int i=row; i>=0; i--){
+//             if(board[i][col] == 'Q'){
+//                 return false;
+//             }
+//         }
+
+//         // upward leftward diagonal
+//         for(int i=row, j=col; i>=0 && j >= 0; i--, j--){
+//             if(board[i][j] == 'Q'){
+//                 return false;
+//             }
+//         }
+
+//         //upward rightward diagonal
+//         for(int i=row, j=col; i>=0 && j < N; i--, j++){
+//             if(board[i][j] == 'Q'){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     void solve(vector<string>&board, int row){
+//         // Base case
+//         if(row >= N){
+//             result.push_back(board);
+//             return;
+//         }
+
+//         for(int col=0; col <N; col++){
+//             if(isValid(board, row, col)){
+//                 board[row][col] = 'Q';
+//                 solve(board, row+1);
+//                 board[row][col] = '.';
+//             }
+//         }
+//     }
+//     vector<vector<string>> solveNQueens(int n) {
+//         N = n;
+//         vector<string>board(n, string(n, '.'));
+//         solve(board, 0);
+
+//         return result;
+//     }
+// };
+
+
+
+//               Optimized version
+
 class Solution {
 public:
     vector<vector<string>>result;
+    unordered_set<int>upward;
+    unordered_set<int>leftward;
+    unordered_set<int>rightward;
     int N ;
-
-    bool isValid(vector<string>&board, int row, int col){
-        // upward
-        for(int i=row; i>=0; i--){
-            if(board[i][col] == 'Q'){
-                return false;
-            }
-        }
-
-        // upward leftward diagonal
-        for(int i=row, j=col; i>=0 && j >= 0; i--, j--){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-
-        //upward rightward diagonal
-        for(int i=row, j=col; i>=0 && j < N; i--, j++){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        return true;
-    }
 
     void solve(vector<string>&board, int row){
         // Base case
@@ -35,10 +71,17 @@ public:
         }
 
         for(int col=0; col <N; col++){
-            if(isValid(board, row, col)){
+            if(upward.find(col) == upward.end() && leftward.find(row-col) == leftward.end() && rightward.find(row+col) == rightward.end()){
                 board[row][col] = 'Q';
+                upward.insert(col);
+                leftward.insert(row-col);
+                rightward.insert(row+col);
                 solve(board, row+1);
+                // backtracking
                 board[row][col] = '.';
+                upward.erase(col);
+                leftward.erase(row-col);
+                rightward.erase(row+col);
             }
         }
     }

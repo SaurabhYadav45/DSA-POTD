a80d5128b203a6dba877ac527114ef5b206c6484
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-23 10:38:13 +0530

    Time: 6 ms (16.22%) | Memory: 15.9 MB (30.60%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -24,7 +24,6 @@
 
 //     void solve(string& curr, int n, int open, int close){
 //         if(curr.length() == 2*n){
-//             ans.push_back(curr);
 //             // if(isValid(curr)){
 //             //     ans.push_back(curr);
 //             // }
@@ -56,21 +55,21 @@
 class Solution {
 public:
     vector<string>ans;
-    void solve(string& curr, int n, int open, int close){
+    void solve(string curr, int n, int open, int close){
         if(curr.length() == 2*n){
             ans.push_back(curr);
         }
 
         if(open < n){
-            curr.push_back('(');
-            solve(curr, n, open+1, close);
-            curr.pop_back();
+            // curr.push_back('(');
+            solve(curr+'(', n, open+1, close);
+            // curr.pop_back();
         }
 
         if(close < open){
-            curr.push_back(')');
-            solve(curr, n, open, close+1);
-            curr.pop_back();
+            // curr.push_back(')');
+            solve(curr+')', n, open, close+1);
+            // curr.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {

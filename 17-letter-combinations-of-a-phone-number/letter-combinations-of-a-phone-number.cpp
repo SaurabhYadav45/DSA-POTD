14a34be7aa4f82266a85aa7c226cc948ca7bb791
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-24 09:23:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (20.48%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,73 @@
 class Solution {
 public:
-void backtrack(const string& d, int i, string& curr,vector<string>& res, const vector<string>& digitToLetters) {
-        if (i == d.size()) {
-            res.push_back(curr);
+    int n ;
+    vector<string>result;
+    void solve(string& s, string& curr, int idx, unordered_map<char, string>&mp){
+        if(idx >= n){
+            result.push_back(curr);
             return;
         }
-        int di = d[i] - '0';
-        const string& l = digitToLetters[di];
-        
-        for (char l : l) {
-            curr.push_back(l);
-            backtrack(d,i+1,curr,res,digitToLetters);
+
+        char i = s[idx];
+        string str = mp[i];
+
+        for(auto&ch: str){
+            curr.push_back(ch);
+            solve(s, curr, idx+1, mp);
             curr.pop_back();
         }
     }
     vector<string> letterCombinations(string d) {
-        if (d.empty()) return {};
-        
-        vector<string> res;
-        string curr;
-        vector<string> digitToLetters = {
-            "", "", "abc", "def", "ghi", "jkl", 
-            "mno", "pqrs", "tuv", "wxyz"
-        };
-        backtrack(d, 0,curr,res,digitToLetters);
-        return res;
+        if(d.empty()) return result;
+        n = d.length();
+        unordered_map<char, string>mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        string curr = "";
+        solve(d, curr, 0, mp);
+        return result;
     }
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+// void backtrack(const string& d, int i, string& curr,vector<string>& res, const vector<string>& digitToLetters) {
+//         if (i == d.size()) {
+//             res.push_back(curr);
+//             return;
+//         }
+//         int di = d[i] - '0';
+//         const string& l = digitToLetters[di];
+        
+//         for (char l : l) {
+//             curr.push_back(l);
+//             backtrack(d,i+1,curr,res,digitToLetters);
+//             curr.pop_back();
+//         }
+//     }
+//     vector<string> letterCombinations(string d) {
+//         if (d.empty()) return {};
+        
+//         vector<string> res;
+//         string curr;
+//         vector<string> digitToLetters = {
+//             "", "", "abc", "def", "ghi", "jkl", 
+//             "mno", "pqrs", "tuv", "wxyz"
+//         };
+//         backtrack(d, 0,curr,res,digitToLetters);
+//         return res;
+//     }
+// };

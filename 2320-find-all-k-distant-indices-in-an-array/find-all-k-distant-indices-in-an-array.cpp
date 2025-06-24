1f2c7f8362bcbe1f496b704e620beb41400ec815
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-24 20:33:10 +0530

    Time: 9 ms (46.97%) | Memory: 14.7 MB (55.05%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -3,17 +3,38 @@ public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
         int n = nums.size();
         vector<int>result;
-        unordered_set<int>st;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(abs(i-j) <= k && nums[j] == key){
-                    st.insert(i);
+                    result.push_back(i);
+                    break;
                 }
             }
         }
-        result.assign(st.begin(), st.end());
-        sort(result.begin(), result.end());
         return result;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+//         int n = nums.size();
+//         vector<int>result;
+//         unordered_set<int>st;
+
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 if(abs(i-j) <= k && nums[j] == key){
+//                     st.insert(i);
+//                 }
+//             }
+//         }
+//         result.assign(st.begin(), st.end());
+//         sort(result.begin(), result.end());
+//         return result;
+//     }
+// };

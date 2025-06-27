728af6ade1bce58cc5cd0350a5e74461ab19a0b3
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-27 22:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (17.03%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -9,22 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void solve(TreeNode* root, vector<int>&nums){
+//         if(root ==  NULL) return ;
+//         if(root->left == NULL && root->right == NULL){
+//             nums.push_back(root->val);
+//         }
+//         solve(root->left, nums);
+//         solve(root->right, nums);
+//     }
+//     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+//         vector<int>nums1;
+//         vector<int>nums2;
+//         solve(root1, nums1);
+//         solve(root2, nums2);
+//         if(nums1 == nums2) return true;
+//         return false;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>&nums){
+    void solve(TreeNode* root, string&s){
         if(root ==  NULL) return ;
         if(root->left == NULL && root->right == NULL){
-            nums.push_back(root->val);
+            s.push_back(root->val);
+            s.push_back('#');
         }
-        solve(root->left, nums);
-        solve(root->right, nums);
+        solve(root->left, s);
+        solve(root->right, s);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>nums1;
-        vector<int>nums2;
-        solve(root1, nums1);
-        solve(root2, nums2);
-        if(nums1 == nums2) return true;
+        string s1;
+        string s2;
+        solve(root1, s1);
+        solve(root2, s2);
+        if(s1 == s2) return true;
         return false;
     }
 };

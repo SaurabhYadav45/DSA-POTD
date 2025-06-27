62d9f57d2774499028961e7aedd82600fc70e146
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-27 22:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (43.73%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -9,10 +9,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int countNodes(TreeNode* root) {
+//         if(root == NULL) return 0;
+//         int left = countNodes(root->left);
+//         int right = countNodes(root->right);
+//         return left + right + 1;
+//     }
+// };
+
+
+
+// Optimized
+
 class Solution {
 public:
+    int countLeftHeight(TreeNode* root){
+        TreeNode* temp = root;
+        int lh = 0;
+        while(temp != NULL){
+           lh++;
+           temp = temp->left; 
+        }
+        return lh;
+    }
+    int countRightHeight(TreeNode* root){
+        TreeNode* temp = root;
+        int rh = 0;
+        while(temp != NULL){
+           rh++;
+           temp = temp->right; 
+        }
+        return rh;
+    }
     int countNodes(TreeNode* root) {
         if(root == NULL) return 0;
+        int lh = countLeftHeight(root);
+        int rh = countRightHeight(root);
+        if(lh == rh){
+            // Perfect Binary tree
+            return pow(2, lh)-1;
+        }
+
         int left = countNodes(root->left);
         int right = countNodes(root->right);
         return left + right + 1;

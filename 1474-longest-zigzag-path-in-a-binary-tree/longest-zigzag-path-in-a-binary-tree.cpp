78c3cb37bed32b0d9b563c532c511098ce1b61a8
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-28 15:34:09 +0530

    Time: 3 ms (64.03%) | Memory: 95.5 MB (54.03%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -9,18 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
     int maxPath = -1;
-    void findLongesZingZaga(TreeNode*root, int left, int right){
+    void findLongesZingZaga(TreeNode*root, int steps, bool goLeft){
         if(root == NULL) return;
-
-        maxPath = max({maxPath, left, right});
-        findLongesZingZaga(root->left, right+1, 0);
-        findLongesZingZaga(root->right, 0, left+1);
+        maxPath = max(maxPath, steps);
+        if(goLeft){
+            findLongesZingZaga(root->right, steps+1, false);
+            findLongesZingZaga(root->left, 1, true); // Explore New path
+        }
+        else{
+            findLongesZingZaga(root->left, steps+1, true);
+            findLongesZingZaga(root->right, 1, false);  // Explore New path
+        }
     }
     int longestZigZag(TreeNode* root) {
-        findLongesZingZaga(root, 0, 0);
+        findLongesZingZaga(root, 0, true);
+        findLongesZingZaga(root, 0, false);
         return maxPath;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxPath = -1;
+//     void findLongesZingZaga(TreeNode*root, int left, int right){
+//         if(root == NULL) return;
+
+//         maxPath = max({maxPath, left, right});
+//         findLongesZingZaga(root->left, right+1, 0);
+//         findLongesZingZaga(root->right, 0, left+1);
+//     }
+//     int longestZigZag(TreeNode* root) {
+//         findLongesZingZaga(root, 0, 0);
+//         return maxPath;
+//     }
+// };

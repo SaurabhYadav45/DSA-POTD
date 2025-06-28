53847933cf57c0d2549ea4ea286c48823aff5fad
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-28 11:31:04 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (14.81%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,24 +12,43 @@
 
 class Solution {
 public:
-    void solve(TreeNode*root, string& s){
-        if(root == NULL){
-            s.push_back('#');
-            return;
-        }
-        s.push_back(root->val);
-        solve(root->left, s);
-        solve(root->right, s);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        string s1;
-        string s2;
-        solve(p, s1);
-        solve(q, s2);
-        if(s1 == s2) return true;
+        if(p == NULL && q == NULL) return true;
+
+        if(p != NULL && q !=  NULL){
+            if((p->val == q->val) &&
+            isSameTree(p->left, q->left) &&
+            isSameTree(p->right, q->right)){
+                return true;
+            }
+        }
         return false;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     void solve(TreeNode*root, string& s){
+//         if(root == NULL){
+//             s.push_back('#');
+//             return;
+//         }
+//         s.push_back(root->val);
+//         solve(root->left, s);
+//         solve(root->right, s);
+//     }
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         string s1;
+//         string s2;
+//         solve(p, s1);
+//         solve(q, s2);
+//         if(s1 == s2) return true;
+//         return false;
+//     }
+// };
 
 
 

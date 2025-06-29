ad18e3896284bd1d5af0effdcb17071b208310eb
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-29 09:12:10 +0530

    Time: 7 ms (20.34%) | Memory: 146.9 MB (47.53%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -9,20 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void findDepth(TreeNode*root, int count, int&result){
+//         if(root == NULL) return;
+//         if(root->left == NULL && root->right == NULL){
+//             result = min(result, count);
+//         }
+//         findDepth(root->left, count+1, result);
+//         findDepth(root->right, count+1, result);
+//     }
+//     int minDepth(TreeNode* root) {
+//         if(root == NULL) return 0;
+//         int result = INT_MAX;
+//         findDepth(root, 1, result);
+//         return result;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void findDepth(TreeNode*root, int count, int&result){
-        if(root == NULL) return;
-        if(root->left == NULL && root->right == NULL){
-            result = min(result, count);
+    int findDepth(TreeNode*root){
+        if(root == NULL) return 0;
+
+        int l = findDepth(root->left);
+        int r = findDepth(root->right);
+        if(root->left == NULL || root->right == NULL){
+            return max(l, r) +1;
         }
-        findDepth(root->left, count+1, result);
-        findDepth(root->right, count+1, result);
+        return min(l, r)+1;
     }
     int minDepth(TreeNode* root) {
         if(root == NULL) return 0;
-        int result = INT_MAX;
-        findDepth(root, 1, result);
-        return result;
+        return findDepth(root);
     }
 };

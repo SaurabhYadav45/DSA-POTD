f2a75d2e1399a670fbe7fb5268c2d159208e93e4
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-30 23:44:52 +0530

    Time: 19 ms (62.43%) | Memory: 44 MB (29.65%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -7,14 +7,19 @@ public:
             mp[num]++;
         }
 
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
 
-        for(int i=0; i<nums.size()-1; i++){
+        for(int i=0; i<nums.size(); i++){
             int mini = nums[i];
-            int maxi = nums[i+1];
-            if(maxi - mini == 1){
-                result = max(result, mp[mini] + mp[maxi]);
+            // int maxi = nums[i+1];
+            int maxi = mini+1;
+            if(mp.find(maxi) == mp.end()){
+                continue;
             }
+            // if(maxi - mini == 1){
+            //     result = max(result, mp[mini] + mp[maxi]);
+            // }
+            result = max(result, mp[mini] + mp[maxi]);
         }
         return result;
     }

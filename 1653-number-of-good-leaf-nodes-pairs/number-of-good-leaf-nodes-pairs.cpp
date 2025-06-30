c6ca6e7d4bb44da4f9d1d6c048e81bc3fb1eb21b
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-30 15:05:54 +0530

    Time: 259 ms (8.79%) | Memory: 118.7 MB (5.93%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
@@ -11,26 +11,24 @@
  */
 class Solution {
 public:
-    void child_to_parent(TreeNode* root, unordered_map<TreeNode*, TreeNode*>&mp, vector<TreeNode*>&leafNode){
+    void createGraph(TreeNode* root, TreeNode*prev, unordered_map<TreeNode*, vector<TreeNode*>>&adj, unordered_set<TreeNode*>&leafNode){
         if(root == NULL) return ;
         if(!root->left && !root->right){
-            leafNode.push_back(root);
+            leafNode.insert(root);
         }
         
-        if(root->left){
-            mp[root->left] = root;
+        if(prev != NULL){
+            adj[prev].push_back(root);
+            adj[root].push_back(prev);
         }
-        if(root->right){
-            mp[root->right] = root;
-        }
-        child_to_parent(root->left, mp, leafNode);
-        child_to_parent(root->right, mp, leafNode);
+        createGraph(root->left, root, adj, leafNode);
+        createGraph(root->right,root, adj, leafNode);
     }
     int countPairs(TreeNode* root, int distance) {
-        unordered_map<TreeNode*, TreeNode*>mp;
-        vector<TreeNode*>leafNode;
+        unordered_map<TreeNode*, vector<TreeNode*>>adj;
+        unordered_set<TreeNode*>leafNode;
 
-        child_to_parent(root, mp, leafNode);
+        createGraph(root,NULL, adj, leafNode);
 
         int count = 0;
 
@@ -38,36 +36,24 @@ public:
             queue<TreeNode*>q;
             unordered_set<TreeNode*>st;
             q.push(leaf);
-            int level = 0;
             st.insert(leaf);
             
-            while(!q.empty()){
+            for(int level=0; level<=distance; level++){
                 int size = q.size();
                 while(size--){
                     TreeNode*node = q.front();
                     q.pop();
 
-                    if(!node->left && !node->right){
-                        if(level != 0 && level <= distance){
-                            count++;
+                    if(node != leaf && leafNode.count(node)){
+                        count++;
+                    }
+                    for(auto& nbr : adj[node]){
+                        if(!st.count(nbr)){
+                            q.push(nbr);
+                            st.insert(nbr);
                         }
                     }
-
-                    if(node->left && st.find(node->left) == st.end()){
-                        q.push(node->left);
-                        st.insert(node->left);
-                    } 
-                    if(node->right && st.find(node->right) == st.end()){
-                        q.push(node->right);
-                        st.insert(node->right);
-                    } 
-                    if(mp.find(node) != mp.end() && st.find(mp[node]) == st.end()){
-                        q.push(mp[node]);
-                        st.insert(mp[node]);
-                    }
                 }
-                level++;
-                if(level > distance) break;
             }
         }
         return count/2;
@@ -79,33 +65,68 @@ public:
 
 
 
+
 // class Solution {
 // public:
-//     int pairs = 0;
-//     bool isLeaf(TreeNode* root) {
-//         return !root->left && !root->right;
-//     }
-//     vector<int> findPairs(TreeNode* root, int distance, int depth) {
-//         if(!root) return {};
-//         if(isLeaf(root)) {
-//             vector<int> dist;
-//             dist.push_back(depth);
-//             return dist;
+//     void child_to_parent(TreeNode* root, unordered_map<TreeNode*, TreeNode*>&mp, vector<TreeNode*>&leafNode){
+//         if(root == NULL) return ;
+//         if(!root->left && !root->right){
+//             leafNode.push_back(root);
 //         }
-//         vector<int> left_dist = findPairs(root->left, distance, depth + 1);
-//         vector<int> right_dist = findPairs(root->right, distance, depth + 1);
-//         for(int ld : left_dist) {
-//             for(int rd : right_dist) {
-//                 pairs += (ld + rd - (2 * depth) <= distance);
-//             }
+        
+//         if(root->left){
+//             mp[root->left] = root;
 //         }
-//         vector<int> res;
-//         for(int ld : left_dist) res.push_back(ld);
-//         for(int rd : right_dist) res.push_back(rd);
-//         return res;
+//         if(root->right){
+//             mp[root->right] = root;
+//         }
+//         child_to_parent(root->left, mp, leafNode);
+//         child_to_parent(root->right, mp, leafNode);
 //     }
 //     int countPairs(TreeNode* root, int distance) {
-//         findPairs(root, distance, 0);
-//         return pairs;
+//         unordered_map<TreeNode*, TreeNode*>mp;
+//         vector<TreeNode*>leafNode;
+
+//         child_to_parent(root, mp, leafNode);
+
+//         int count = 0;
+
+//         for(auto& leaf: leafNode){
+//             queue<TreeNode*>q;
+//             unordered_set<TreeNode*>st;
+//             q.push(leaf);
+//             int level = 0;
+//             st.insert(leaf);
+            
+//             while(!q.empty()){
+//                 int size = q.size();
+//                 while(size--){
+//                     TreeNode*node = q.front();
+//                     q.pop();
+
+//                     if(!node->left && !node->right){
+//                         if(level != 0 && level <= distance){
+//                             count++;
+//                         }
+//                     }
+
+//                     if(node->left && st.find(node->left) == st.end()){
+//                         q.push(node->left);
+//                         st.insert(node->left);
+//                     } 
+//                     if(node->right && st.find(node->right) == st.end()){
+//                         q.push(node->right);
+//                         st.insert(node->right);
+//                     } 
+//                     if(mp.find(node) != mp.end() && st.find(mp[node]) == st.end()){
+//                         q.push(mp[node]);
+//                         st.insert(mp[node]);
+//                     }
+//                 }
+//                 level++;
+//                 if(level > distance) break;
+//             }
+//         }
+//         return count/2;
 //     }
 // };

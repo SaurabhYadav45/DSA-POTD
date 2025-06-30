2fb61041af5ad70c61fee8371a59a05d78fa7fe4
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-30 11:36:14 +0530

    Time: 9 ms (41.53%) | Memory: 158 MB (32.65%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -9,45 +9,84 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     bool isEvenOddTree(TreeNode* root) {
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         bool even = true;
+
+//         while(!q.empty()){
+//             int size = q.size();
+//             int prev;
+//             if(even == true){
+//                 prev = INT_MIN;
+//             }
+//             else{
+//                 prev = INT_MAX;
+//             }
+//             while(size--){
+//                 TreeNode* node = q.front();
+//                 q.pop();
+//                 if(even){
+//                     if(node->val % 2 == 0 || prev >= node->val){
+//                         return false;
+//                     }
+//                 }
+//                 else{
+//                     if(node->val % 2 != 0 || prev <= node->val){
+//                         return false;
+//                     }
+//                 }
+//                 if(node->left){
+//                     q.push(node->left);
+//                 }
+//                 if(node->right){
+//                     q.push(node->right);
+//                 }
+//                 prev = node->val;
+//             }
+//             even = !even;
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+//                 ******************  Using DFS **************************
+
 class Solution {
 public:
-    bool isEvenOddTree(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        bool even = true;
+    bool solve(TreeNode* root, int level, vector<int>&levelPrev){
+        if(root == NULL) return true;
 
-        while(!q.empty()){
-            int size = q.size();
-            int prev;
-            if(even == true){
-                prev = INT_MIN;
-            }
-            else{
-                prev = INT_MAX;
-            }
-            while(size--){
-                TreeNode* node = q.front();
-                q.pop();
-                if(even){
-                    if(node->val % 2 == 0 || prev >= node->val){
-                        return false;
-                    }
-                }
-                else{
-                    if(node->val % 2 != 0 || prev <= node->val){
-                        return false;
-                    }
-                }
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-                prev = node->val;
-            }
-            even = !even;
+        if(level % 2 == root->val % 2){
+            return false;
         }
-        return true;
+
+        if(level >= levelPrev.size()){
+            levelPrev.resize(level+1);
+        }
+
+        if(levelPrev[level] != 0){
+            if(level % 2 == 0 && levelPrev[level] >= root->val){
+                return false;
+            }
+            if(level % 2 != 0 && levelPrev[level] <= root->val){
+                return false;
+            }
+        }
+
+        levelPrev[level] = root->val;
+        
+        bool l = solve(root->left, level+1, levelPrev);
+        bool r = solve(root->right, level+1, levelPrev);
+        return (l && r);
+    }
+    bool isEvenOddTree(TreeNode* root) {
+        vector<int>levelPrev;
+        return solve(root, 0, levelPrev);
     }
 };

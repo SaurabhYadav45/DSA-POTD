cae25c0a53aa4dfd94df6ff32e4662392c663319
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-06-30 09:11:16 +0530

    Time: 3 ms (20.59%) | Memory: 25.2 MB (25.42%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -9,66 +9,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//                  ***********Using  DFS *************
+
 // class Solution {
 // public:
-//     int findLevel(TreeNode* root){
-//         if(root == NULL) return 0;
-//         int l = findLevel(root->left);
-//         int r = findLevel(root->right);
-//         return max(l, r) +1;
+//     void solve(TreeNode* root, int currCount, int& levelCnt, int& result){
+//         if(root == NULL) return;
+//         if(root->left == NULL && root->right == NULL){
+//             if(currCount > levelCnt){
+//                 result = root->val;
+//                 levelCnt = currCount;
+//                 return;
+//             }
+//         }
+//         solve(root->left, currCount + 1, levelCnt, result);
+//         solve(root->right, currCount + 1, levelCnt, result);
 //     }
 //     int findBottomLeftValue(TreeNode* root) {
-//         int level = findLevel(root);
-//         int result = 0;
-//         queue<TreeNode*>q;
-//         q.push(root);
-//         int count = 1;
-//         while(!q.empty()){
-//             int size = q.size();
-//             if(count == level){
-//                 result = q.front()->val;
-//                 break;
-//             }
-//             while(size--){
-//                 TreeNode* node = q.front();
-//                 q.pop();
-
-//                 if(node->left){
-//                     q.push(node->left);
-//                 }
-//                 if(node->right){
-//                     q.push(node->right);
-//                 }
-//             }
-//             count++;
+//         if(root->left == NULL && root->right == NULL){
+//             return root->val;
 //         }
-//         return result; 
+//         int result = 0;
+//         int levelCnt = 0;
+//         solve(root, 0, levelCnt, result);
+//         return result;
 //     }
 // };
 
 
 
+
+
+//                  ***********Using  BFS *************
+
 class Solution {
 public:
-    void solve(TreeNode* root, int currCount, int& levelCnt, int& result){
-        if(root == NULL) return;
-        if(root->left == NULL && root->right == NULL){
-            if(currCount > levelCnt){
-                result = root->val;
-                levelCnt = currCount;
-                return;
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+
+        TreeNode* node;
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                node = q.front();
+                q.pop();
+
+                if(node->right){
+                    q.push(node->right);
+                }
+                if(node->left){
+                    q.push(node->left);
+                }
             }
         }
-        solve(root->left, currCount + 1, levelCnt, result);
-        solve(root->right, currCount + 1, levelCnt, result);
-    }
-    int findBottomLeftValue(TreeNode* root) {
-        if(root->left == NULL && root->right == NULL){
-            return root->val;
-        }
-        int result = 0;
-        int levelCnt = 0;
-        solve(root, 0, levelCnt, result);
-        return result;
+        return node->val; 
     }
 };

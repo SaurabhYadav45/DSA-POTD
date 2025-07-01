a0a89caf838c71840bc9c9db400c59ff8b6b42e2
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-01 19:06:36 +0530

    Time: 2 ms (48.46%) | Memory: 8.7 MB (83.47%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,17 +1,32 @@
+// class Solution {
+// public:
+//     int possibleStringCount(string word) {
+//         int n = word.length();
+//         int count = 0;
+//         int i=0, j=1;
+//         while(j < n){
+//             if(word[j] == word[i]){
+//                 count++;
+//             }
+//             else{
+//                 i = j;
+//             }
+//             j++;
+//         }
+//         return count+1;
+//     }
+// };
+
+
 class Solution {
 public:
     int possibleStringCount(string word) {
         int n = word.length();
         int count = 0;
-        int i=0, j=1;
-        while(j < n){
-            if(word[j] == word[i]){
+        for(int i=1; i<n; i++){
+            if(word[i] == word[i-1]){
                 count++;
             }
-            else{
-                i = j;
-            }
-            j++;
         }
         return count+1;
     }
@@ -20,26 +35,3 @@ public:
 
 
 
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int possibleStringCount(string word) {
-//         vector<int>freq(26, 0);
-//         for(char& ch : word){
-//             freq[ch-'a']++;
-//         }
-
-//         int count = 0;
-//         for(int& f : freq){
-//             if(f == 0) continue;
-//             count += f-1;
-//         }
-//         return count+1;
-//     }
-// };

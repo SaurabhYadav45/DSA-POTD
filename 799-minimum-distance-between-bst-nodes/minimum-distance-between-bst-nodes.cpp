382c6085f7287ac986e6ecc83f5d051d13df6d7f
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-01 11:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (59.46%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -9,24 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     TreeNode* prev;
+//     int minDiffInBST(TreeNode* root) {
+//         if(root == NULL) return INT_MAX;
+
+//         int ans = INT_MAX;
+//         int left = minDiffInBST(root->left);
+//         ans = min(ans, left);
+
+//         if(prev != NULL){
+//             ans = min(ans, root->val - prev->val);
+//         }
+//         prev = root;
+
+//         int right = minDiffInBST(root->right);
+//         ans = min(ans, right);
+
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     TreeNode* prev;
-    int minDiffInBST(TreeNode* root) {
-        if(root == NULL) return INT_MAX;
-
-        int ans = INT_MAX;
-        int left = minDiffInBST(root->left);
-        ans = min(ans, left);
+    int ans = INT_MAX;
+    void solve(TreeNode* root){
+        if(root == NULL) return;
+        
+        solve(root->left);
 
         if(prev != NULL){
             ans = min(ans, root->val - prev->val);
         }
         prev = root;
 
-        int right = minDiffInBST(root->right);
-        ans = min(ans, right);
-
+        solve(root->right);
+    }
+    int minDiffInBST(TreeNode* root) {
+        solve(root);
         return ans;
     }
 };

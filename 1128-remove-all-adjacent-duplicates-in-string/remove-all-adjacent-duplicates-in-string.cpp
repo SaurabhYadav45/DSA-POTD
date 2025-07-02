fcf6d4887a91cbd46a700026c18aa387248b27bf
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-02 11:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (28.98%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,16 +1,44 @@
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         string result = "";
+//         result.push_back(s[0]);
+//         for(int i=1; i<s.length(); i++){
+//             if(!result.empty() && s[i] == result.back()){
+//                 result.pop_back();
+//             }
+//             else{
+//                 result.push_back(s[i]);
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
+
+//              **************** Stack *****************
+
+
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string result = "";
-        result.push_back(s[0]);
-        for(int i=1; i<s.length(); i++){
-            if(!result.empty() && s[i] == result.back()){
-                result.pop_back();
+        stack<char>st;
+        for(int i=0; i<s.length(); i++){
+            if(!st.empty() && s[i] == st.top()){
+                st.pop();
             }
             else{
-                result.push_back(s[i]);
+                st.push(s[i]);
             }
         }
+
+        string result = "";
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
         return result;
     }
 };

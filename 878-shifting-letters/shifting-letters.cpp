862865da9807757209e5f7ba24e59f2ce1203c5e
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-03 20:49:22 +0530

    Time: 5 ms (50.39%) | Memory: 77.1 MB (24.19%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = s.length();
+        vector<long long>pSum(n, 0);
+        pSum[n-1] = shifts[n-1];
+        for(int i=n-2; i>=0; i--){
+            pSum[i] = (long long)pSum[i+1] + shifts[i];
+        }
+
+        for(int i=0; i<n; i++){
+            s[i] = (s[i] - 'a' + pSum[i]) % 26 + 'a';
+        }
+        return s;
+    }
+};

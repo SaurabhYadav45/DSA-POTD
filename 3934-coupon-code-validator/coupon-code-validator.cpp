97b02f5c91fd4843966cb3b054aa168de7264fdc
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-06 20:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 69.2 MB (76.94%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -1,3 +1,52 @@
+// class Solution {
+// public:
+//     bool isValid(string& s){
+//         for(int i=0; i<s.length(); i++){
+//             char ch = s[i];
+//             if(!isalnum(ch) && !(ch == '_')){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     // static bool myComp(pair<string, string>&a, pair<string, string>&b){
+//     //     if(a.second == b.second){
+//     //         return a.first < b.first;
+//     //     }
+//     //     return a.second < b.second;
+//     // }
+//     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+//         auto  lambda = [](pair<string, string>&a, pair<string, string>&b){
+//             if(a.second == b.second){
+//                 return a.first < b.first;
+//             }
+//             return a.second < b.second;
+//         };
+//         unordered_set<string>st;
+//         st.insert("electronics");
+//         st.insert("grocery");
+//         st.insert("pharmacy");
+//         st.insert("restaurant");
+
+//         vector<string>result;
+//         vector<pair<string, string>>pairs;
+
+//         for(int i=0; i<code.size(); i++){
+//             if(code[i].empty()) continue;
+//             if(isValid(code[i]) && st.find(businessLine[i]) != st.end() && isActive[i] == true){
+//                 pairs.push_back({code[i], businessLine[i]});
+//             }
+//         }
+//         sort(pairs.begin(), pairs.end(), lambda);
+//         for(auto& pair: pairs){
+//             result.push_back(pair.first);
+//         }
+//         return result;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool isValid(string& s){
@@ -9,18 +58,12 @@ public:
         }
         return true;
     }
-    // static bool myComp(pair<string, string>&a, pair<string, string>&b){
-    //     if(a.second == b.second){
-    //         return a.first < b.first;
-    //     }
-    //     return a.second < b.second;
-    // }
     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-        auto  lambda = [](pair<string, string>&a, pair<string, string>&b){
-            if(a.second == b.second){
+        auto  lambda = [&](pair<string, int>&a, pair<string, int>&b){
+            if(businessLine[a.second] == businessLine[b.second]){
                 return a.first < b.first;
             }
-            return a.second < b.second;
+            return businessLine[a.second] < businessLine[b.second];
         };
         unordered_set<string>st;
         st.insert("electronics");
@@ -29,12 +72,12 @@ public:
         st.insert("restaurant");
 
         vector<string>result;
-        vector<pair<string, string>>pairs;
+        vector<pair<string, int>>pairs;
 
         for(int i=0; i<code.size(); i++){
             if(code[i].empty()) continue;
             if(isValid(code[i]) && st.find(businessLine[i]) != st.end() && isActive[i] == true){
-                pairs.push_back({code[i], businessLine[i]});
+                pairs.push_back({code[i], i});
             }
         }
         sort(pairs.begin(), pairs.end(), lambda);

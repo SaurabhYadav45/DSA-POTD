918e9da1218b467de1d6b154196eaea3605dce07
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-08 10:21:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (29.37%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -19,63 +19,41 @@
 
 //              ***************** Using Queue ****************
 
+// class Solution {
+// public:
+//     int findTheWinner(int n, int k) {
+//         queue<int>q;
+//         for(int i=1; i<=n; i++){
+//             q.push(i);
+//         }
+
+//         while(q.size() != 1){
+//             for(int i=1; i<k; i++){
+//                 q.push(q.front());
+//                 q.pop();
+//             }
+//             q.pop();
+//         }
+//         return q.front();
+//     }
+// };
+
+
+//              ***************** Using Array ****************
+
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int>q;
+        vector<int>nums;
         for(int i=1; i<=n; i++){
-            q.push(i);
+            nums.push_back(i);
         }
 
-        while(q.size() != 1){
-            for(int i=1; i<k; i++){
-                q.push(q.front());
-                q.pop();
-            }
-            q.pop();
+        int idx=0;
+        while(nums.size() > 1){
+            idx = (idx + k -1) % nums.size();
+            nums.erase(nums.begin()+idx);
         }
-        return q.front();
+        return nums[0];
     }
 };
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int findWinnerIndex(int n, int k){
-//         // base case
-//         if(n == 0){
-//             return 0; // index
-//         }
-
-//         int idx = findWinnerIndex(n-1, k);
-//         idx = (idx+k)%n;  // To find the original index in the original array
-
-//         return idx;
-//     }
-
-//     int findTheWinner(int n, int k) {
-
-//         int idx = findWinnerIndex(n, k);
-//         return idx+1;
-
-//     //     //  Method -2 : Using Queue
-
-// //             queue<int>q;
-// //             for(int i=1; i<=n; i++)
-// //                 q.push(i);
-            
-// //             while(q.size() > 1){
-// //                 for(int i = 1; i<k; i++){
-// //                     auto player = q.front();
-// //                     q.pop();
-// //                     q.push(player);
-// //                 }
-// //                 q.pop();
-// //             }
-// //             return q.front();
-//         }
-//     };

7dddfdaa8ff2f30f6cc3a9fa497b622c11c88029
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-10 09:52:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.07%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,53 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int row = matrix.size();
+//         int col = matrix[0].size();
+//         int n = row*col;
+
+//         int s = 0;
+//         int e = n-1;
+//         int mid = s + (e-s)/2;
+//         while(s<=e){
+//             int rowIndex = mid/col;
+//             int colIndex = mid%col;
+//             int currNumber = matrix[rowIndex][colIndex];
+//             if(currNumber == target){
+//                 return true;
+//             }
+//             else if(currNumber > target){
+//                 e = mid-1;
+//             }
+//             else{
+//                 s = mid + 1;
+//             }
+//             mid = s + (e-s)/2;
+//         }
+//         return false;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int l = 0, r = m*n-1;
+        int mid;
+        while(l <= r){
+            mid = l + (r-l)/2;
+            if(matrix[mid/n][mid%n] < target){
+                l = mid+1;
+            }
+            else if(matrix[mid/n][mid%n] > target){
+                r = mid-1;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};

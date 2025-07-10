23c18a7e0c0649d74766678389abe9c412414cac
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-10 09:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (12.36%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -5,21 +5,15 @@ public:
         int l = 0;
         int r = n-1;
         int mid;
-        int result = INT_MAX;
-        while(l <= r){
+        while(l < r){
             mid = l+(r-l)/2;
             if(nums[mid] > nums[r]){
                 l = mid+1;
             }
-            else if(nums[mid] < nums[r]){
-                result = min(result, nums[mid]);
-                r = mid-1;
-            }
             else{
-                result = min(result, nums[mid]);
-                break;
+                r = mid;
             }
         }
-        return result;
+        return nums[l];
     }
 };

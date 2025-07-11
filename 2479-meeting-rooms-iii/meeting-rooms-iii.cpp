6cbdbadbc848c560717893636a37c0a756cf14e9
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-11 23:24:34 +0530

    Time: 66 ms (85.55%) | Memory: 98.3 MB (56.92%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,39 +1,92 @@
+// class Solution {
+// public:
+//     int mostBooked(int n, vector<vector<int>>& meetings) {
+//         sort(meetings.begin(), meetings.end());
+//         vector<long long>roomAvailableAt(n, 0);
+//         vector<int>roomUsedCount(n, 0);
+        
+//         for(auto& meet: meetings){
+//             int start = meet[0];
+//             int end = meet[1];
+//             int duration = end - start;
+
+//             long long earlyEndRoomTime = LLONG_MAX;
+//             int earlyEndRoom = 0;
+//             bool found = false;
+//             for(int room=0; room < n; room++){
+//                 if(roomAvailableAt[room] <= start){
+//                     roomAvailableAt[room] = end;
+//                     roomUsedCount[room]++;
+//                     found = true;
+//                     break;
+//                 }
+
+//                 if(roomAvailableAt[room] < earlyEndRoomTime){
+//                     earlyEndRoomTime = roomAvailableAt[room];
+//                     earlyEndRoom = room;
+//                 }
+//             }
+//             if(!found){
+//                 long long newEndTime = earlyEndRoomTime + duration;
+//                 roomAvailableAt[earlyEndRoom] = newEndTime;
+//                 roomUsedCount[earlyEndRoom]++;
+//             }
+//         }
+
+//         int maxCount = 0;
+//         int resultRoom = -1;
+//         for(int room =0; room<n; room++){
+//             if(roomUsedCount[room] > maxCount){
+//                 maxCount = roomUsedCount[room];
+//                 resultRoom = room;
+//             }
+//         }
+//         return resultRoom;
+//     }
+// };
+
+
+
+//           *************** Optimal (Min Heap) ***************
+
 class Solution {
 public:
+    typedef pair<long long, int> P;
     int mostBooked(int n, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
-        vector<long long>roomAvailableAt(n, 0);
-        vector<int>roomUsedCount(n, 0);
+        vector<long long>roomUsedCount(n, 0);
+        priority_queue<P, vector<P>, greater<P>>roomUsed;
+        priority_queue<int, vector<int>, greater<int>>availableRoom;
+
+        for(int room=0; room<n; room++){
+            availableRoom.push(room);
+        }
         
-        for(auto& meet: meetings){
+        for(vector<int>& meet: meetings){
             int start = meet[0];
             int end = meet[1];
-            int duration = end - start;
-
-            long long earlyEndRoomTime = LLONG_MAX;
-            int earlyEndRoom = 0;
-            bool found = false;
-            for(int room=0; room < n; room++){
-                if(roomAvailableAt[room] <= start){
-                    roomAvailableAt[room] = end;
-                    roomUsedCount[room]++;
-                    found = true;
-                    break;
-                }
-
-                if(roomAvailableAt[room] < earlyEndRoomTime){
-                    earlyEndRoomTime = roomAvailableAt[room];
-                    earlyEndRoom = room;
-                }
+            long long duration = end-start;
+            
+            while(!roomUsed.empty() && roomUsed.top().first <= start){
+                availableRoom.push(roomUsed.top().second);
+                roomUsed.pop();
             }
-            if(!found){
-                long long newEndTime = earlyEndRoomTime + duration;
-                roomAvailableAt[earlyEndRoom] = newEndTime;
-                roomUsedCount[earlyEndRoom]++;
+            if(!availableRoom.empty()){
+                int room = availableRoom.top();
+                availableRoom.pop();
+                roomUsed.push({end, room});
+                roomUsedCount[room]++;
+            }
+            else{
+                // room is not available, pick the smallest end time
+                int room = roomUsed.top().second;
+                long long endTime = roomUsed.top().first;
+                roomUsed.pop();
+                roomUsed.push({endTime + duration, room});
+                roomUsedCount[room]++;
             }
         }
-
-        int maxCount = 0;
+        int maxCount = -1;
         int resultRoom = -1;
         for(int room =0; room<n; room++){
             if(roomUsedCount[room] > maxCount){

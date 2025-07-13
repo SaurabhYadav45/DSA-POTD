60d3ca7c88a88f5138955242ac9f181584059271
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-13 21:16:40 +0530

    Time: 15 ms (97.82%) | Memory: 24.1 MB (89.73%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     char processStr(string s, long long k) {
-        long long n = 0;
+        long long n = 0;  // final length of string
+
+        // lets calculate the final length of the string
         for(char&ch : s){
             if(ch == '*') n = max((long long)0, n-1);
             else if(ch == '#') n = n > 0 ? n*2: 0;
             else if(ch == '%') continue;
             else n++;
         }
+
+        // check if length of final string is less than equal to k, if yes  return '.'
         if( n <= k) return '.';
+        // reverse it so that we can perform backward simulation from the starting index (Optional)
         reverse(s.begin(), s.end());
 
+        // perform backward operation
         for(char& ch: s){
             if(ch == '*') n++; 
             else if(ch == '#'){

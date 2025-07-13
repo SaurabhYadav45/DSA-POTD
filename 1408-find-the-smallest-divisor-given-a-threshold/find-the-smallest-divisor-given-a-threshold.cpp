bc3801f8154059e116ae9d40700538a360c1f3f0
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-13 12:07:03 +0530

    Time: 12 ms (37.89%) | Memory: 26.3 MB (16.18%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums, int threshold, int divisor){
+        int sum = 0;
+        for(int& num: nums){
+            sum += ceil((float)num/divisor);
+        }
+        if(sum <= threshold){
+            return true;
+        }
+        return false;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int result = -1;
+        int low = 1;
+        int high = *max_element(nums.begin(), nums.end());
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(isPossible(nums, threshold, mid)){
+                result = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return result;
+    }
+};
+
+
+
+//          ************** Brute Force ***********
+
+// class Solution {
+// public:
+//     int smallestDivisor(vector<int>& nums, int threshold) {
+//         int result = -1;
+//         int high = *max_element(nums.begin(), nums.end());
+//         for(int divisor=1; divisor<=high; divisor++){
+//             int sum = 0;
+//             for(int& num: nums){
+//                 sum += ceil((float)num/divisor);
+//             }
+//             if(sum <= threshold){
+//                 result = divisor;
+//                 break;
+//             }
+//         }
+//         return result;
+//     }
+// };

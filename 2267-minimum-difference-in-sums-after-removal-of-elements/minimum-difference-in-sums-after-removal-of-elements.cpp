8af0ec8957aa5edf164411704ccdfe30d19206ef
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-18 21:27:56 +0530

    Time: 142 ms (60.05%) | Memory: 154.8 MB (44.85%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -1,16 +1,65 @@
+// class Solution {
+// public:
+//     // #define ll long long;
+//     long long minimumDifference(vector<int>& nums) {
+//         int n = nums.size()/3;
+//         priority_queue<long long>maxHeap;
+//         priority_queue<long long, vector<long long>, greater<long long>>minHeap;
+//         vector<long long>prefix(nums.size(), -1);
+//         vector<long long>suffix(nums.size(), -1);
+//         long long sum =0;
+//         for(int i=0; i<nums.size(); i++){
+//             sum += nums[i];
+//             maxHeap.push(nums[i]);
+
+//             if(maxHeap.size() > n){
+//                 sum -= maxHeap.top();
+//                 maxHeap.pop();
+//             }
+
+//             if(maxHeap.size() == n){
+//                 prefix[i] = sum;
+//             }
+//         }
+//         sum = 0;
+//         for(int i=nums.size()-1; i>=0; i--){
+//             sum += nums[i];
+//             minHeap.push(nums[i]);
+
+//             if(minHeap.size() > n){
+//                 sum -= minHeap.top();
+//                 minHeap.pop();
+//             }
+
+//             if(minHeap.size() == n){
+//                 suffix[i] = sum;
+//             }
+//         }
+//         long long ans = LONG_LONG_MAX;
+//         for(int i=n-1; i<2*n;i++){
+//             ans  = min(ans, prefix[i] - suffix[i+1]);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    // #define ll long long;
+    typedef long long ll;
     long long minimumDifference(vector<int>& nums) {
-        int n = nums.size()/3;
-        priority_queue<long long>maxHeap;
-        priority_queue<long long, vector<long long>, greater<long long>>minHeap;
-        vector<long long>prefix(nums.size(), -1);
-        vector<long long>suffix(nums.size(), -1);
-        long long sum =0;
-        for(int i=0; i<nums.size(); i++){
-            sum += nums[i];
+        int N = nums.size();
+        int n = N/3;
+        priority_queue<int>maxHeap;
+        priority_queue<int, vector<int>, greater<int>>minHeap;
+        vector<ll>leftMin(N, 0);
+        vector<ll>rightMax(N, 0);
+
+        ll sum = 0;
+        for(int i=0; i<2*n; i++){
             maxHeap.push(nums[i]);
+            sum += nums[i];
 
             if(maxHeap.size() > n){
                 sum -= maxHeap.top();
@@ -18,13 +67,15 @@ public:
             }
 
             if(maxHeap.size() == n){
-                prefix[i] = sum;
+                leftMin[i] = sum; 
             }
         }
+
+
         sum = 0;
-        for(int i=nums.size()-1; i>=0; i--){
-            sum += nums[i];
+        for(int i=N-1; i>=n; i--){
             minHeap.push(nums[i]);
+            sum += nums[i];
 
             if(minHeap.size() > n){
                 sum -= minHeap.top();
@@ -32,13 +83,14 @@ public:
             }
 
             if(minHeap.size() == n){
-                suffix[i] = sum;
+                rightMax[i] = sum; 
             }
         }
-        long long ans = LONG_LONG_MAX;
-        for(int i=n-1; i<2*n;i++){
-            ans  = min(ans, prefix[i] - suffix[i+1]);
+
+        ll result = LLONG_MAX;
+        for(int i=n-1; i<2*n; i++){
+            result = min(result, leftMin[i]-rightMax[i+1]);
         }
-        return ans;
+        return result;
     }
 };

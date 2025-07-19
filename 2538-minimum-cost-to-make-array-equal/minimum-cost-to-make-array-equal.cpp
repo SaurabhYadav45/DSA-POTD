13f1b2cc5c48fb2479101b57b34532e612df8f01
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-19 09:20:59 +0530

    Time: 13 ms (53.55%) | Memory: 45.6 MB (27.19%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
@@ -1,28 +1,63 @@
+// class Solution {
+// public:
+//     long long findCost(vector<int>& nums, vector<int>& cost, long long mid){
+//         long long calCost = 0;
+//         for(int i=0; i<nums.size(); i++){
+//             calCost += abs(nums[i] - mid)*cost[i];
+//         }
+//         return calCost;
+//     }
+//     long long minCost(vector<int>& nums, vector<int>& cost) {
+//         long long result = INT_MAX;
+//         int low = *min_element(nums.begin(), nums.end());
+//         int high = *max_element(nums.begin(), nums.end());
+//         while(low <= high){
+//             long long mid = low + (high-low)/2;
+//             long long cost1 = findCost(nums, cost, mid);
+//             long long cost2 = findCost(nums, cost, mid+1);
+
+//             result = min(cost1, cost2);
+//             if(cost2 > cost1){
+//                 high = mid-1;
+//             }
+//             else{
+//                 low = mid+1;
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
+
+//
 class Solution {
 public:
-    long long findCost(vector<int>& nums, vector<int>& cost, long long mid){
-        long long calCost = 0;
-        for(int i=0; i<nums.size(); i++){
-            calCost += abs(nums[i] - mid)*cost[i];
-        }
-        return calCost;
-    }
+    typedef long long ll;
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        long long result = INT_MAX;
-        int low = *min_element(nums.begin(), nums.end());
-        int high = *max_element(nums.begin(), nums.end());
-        while(low <= high){
-            long long mid = low + (high-low)/2;
-            long long cost1 = findCost(nums, cost, mid);
-            long long cost2 = findCost(nums, cost, mid+1);
+        int n = nums.size();
+        vector<pair<int, int>>pairs;
+        ll totalCost = 0;
+        for(int i=0; i<n; i++){
+            pairs.push_back({nums[i], cost[i]});
+            totalCost += cost[i];
+        }
+        sort(pairs.begin(), pairs.end());
 
-            result = min(cost1, cost2);
-            if(cost2 > cost1){
-                high = mid-1;
+        // Find weighted median
+        ll median = 0;
+        ll accumulated = 0;
+        for(auto& p : pairs){
+            accumulated += p.second;
+            if(accumulated >= (totalCost+1)/2){
+                median = p.first;
+                break;
             }
-            else{
-                low = mid+1;
-            }
+        }
+
+        ll result = 0;
+        for(int i=0; i<n; i++){
+            result += abs(nums[i]-median)*cost[i];
         }
         return result;
     }

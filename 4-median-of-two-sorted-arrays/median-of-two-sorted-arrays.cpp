4afd0baa07e82316f128c59f9e0b183c32e3d29a
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-20 10:22:25 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (78.83%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,91 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>nums;
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int i=0, j=0;
-        while(i < n1 && j <n2){
+        int m = nums1.size();
+        int n = nums2.size();
+        int i=0, j=0, k=0;
+
+        int idx1 = (m+n)/2-1;
+        int idx2 = (m+n)/2;
+        int element1 = 0;
+        int element2 = 0;
+        while(i < m && j <n){
             if(nums1[i] < nums2[j]){
-                nums.push_back(nums1[i]);
+                if(k == idx1) element1 = nums1[i];
+                if(k == idx2) element2 = nums1[i];
                 i++;
             }
             else{
-                nums.push_back(nums2[j]);
+                if(k == idx1) element1 = nums2[j];
+                if(k == idx2) element2 = nums2[j];
                 j++;
             }
+            k++;
         }
 
-        while( i < n1){
-            nums.push_back(nums1[i]);
+        while( i < m){
+            if(k == idx1) element1 = nums1[i];
+            if(k == idx2) element2 = nums1[i];
             i++;
+            k++;
         }
-        while( j < n2){
-            nums.push_back(nums2[j]);
+        while( j < n){
+            if(k == idx1) element1 = nums2[j];
+            if(k == idx2) element2 = nums2[j];
             j++;
+            k++;
         }
         
-        int n = nums.size();
         double median=0;
-        if(nums.size() % 2 != 0){
-            median = (double)nums[n/2];
+        if((m+n) % 2 != 0){
+            median = (double)element2;
         }
         else{
-           median = (double)(nums[(n/2)] + nums[(n/2)-1])/2;
+           median = (double)(element1 + element2)/2;
         }
         return median;
     }
 };
+
+
+
+//         ******************* Brute Force *******************
+
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int>nums;
+//         int n1 = nums1.size();
+//         int n2 = nums2.size();
+//         int i=0, j=0;
+//         while(i < n1 && j <n2){
+//             if(nums1[i] < nums2[j]){
+//                 nums.push_back(nums1[i]);
+//                 i++;
+//             }
+//             else{
+//                 nums.push_back(nums2[j]);
+//                 j++;
+//             }
+//         }
+
+//         while( i < n1){
+//             nums.push_back(nums1[i]);
+//             i++;
+//         }
+//         while( j < n2){
+//             nums.push_back(nums2[j]);
+//             j++;
+//         }
+        
+//         int n = nums.size();
+//         double median=0;
+//         if(nums.size() % 2 != 0){
+//             median = (double)nums[n/2];
+//         }
+//         else{
+//            median = (double)(nums[(n/2)] + nums[(n/2)-1])/2;
+//         }
+//         return median;
+//     }
+// };

71717f2de8d573c836938e468601e60e16259051
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-21 13:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (33.99%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,18 +1,57 @@
 class Solution {
 public:
+    int slidingWindowCount(vector<int>& nums, int mid){
+        int i=0;
+        int j = 1;
+        int count = 0;
+        while(j < nums.size()){
+            while(nums[j]-nums[i] > mid){
+                i++;
+            }
+            count += j-i;
+            j++;
+        }
+        return count;
+    }
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int>vec;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                vec.push_back(abs(nums[j]-nums[i]));
+        sort(nums.begin(), nums.end());
+
+        int l = 0;
+        int h = nums[n-1]- nums[0];
+        int result = 0;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            int count = slidingWindowCount(nums, mid);
+            if(count < k){
+                l = mid+1;
+            }
+            else{
+                result = mid;
+                h = mid-1;
             }
         }
-
-        nth_element(vec.begin(), vec.begin()+(k-1), vec.end());
-        return vec[k-1];
+        return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int smallestDistancePair(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int>vec;
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 vec.push_back(abs(nums[j]-nums[i]));
+//             }
+//         }
+
+//         nth_element(vec.begin(), vec.begin()+(k-1), vec.end());
+//         return vec[k-1];
+//     }
+// };
 
 
 

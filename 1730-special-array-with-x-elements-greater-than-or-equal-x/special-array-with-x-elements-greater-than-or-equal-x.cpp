ba3410b4fa6194930a3724908d00133c2e4da77e
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-21 09:43:52 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (6.81%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -2,25 +2,51 @@ class Solution {
 public:
     int specialArray(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int l = 0, h = n;
-
-        while(l <= h){
-            int mid = l + (h-l)/2;
-            int i = lower_bound(nums.begin(), nums.end(), mid)- nums.begin();
-            if(n-i > mid){
-                l = mid+1;
-            }
-            else if(n-i < mid){
-                h = mid-1;
+        vector<int>freq(n+1, 0);
+        for(int i=0; i<n; i++){
+            if(nums[i] >= n){
+                freq[n]++;
             }
             else{
-                return mid;
+                freq[nums[i]]++;
             }
+        }
+
+        if(freq[n] == n) return n;
+        for(int i=n-1; i>=0; i--){
+            freq[i] += freq[i+1];
+            if(freq[i] == i) return i;
         }
         return -1;
     }
 };
+
+
+//           **************** Binary Search ***********
+
+// class Solution {
+// public:
+//     int specialArray(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+//         int l = 0, h = n;
+
+//         while(l <= h){
+//             int mid = l + (h-l)/2;
+//             int i = lower_bound(nums.begin(), nums.end(), mid)- nums.begin();
+//             if(n-i > mid){
+//                 l = mid+1;
+//             }
+//             else if(n-i < mid){
+//                 h = mid-1;
+//             }
+//             else{
+//                 return mid;
+//             }
+//         }
+//         return -1;
+//     }
+// };
 
 
 //       **************** Brute Force **************

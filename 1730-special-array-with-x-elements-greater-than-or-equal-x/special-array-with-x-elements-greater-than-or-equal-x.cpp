2e49ef4353f0a73d7a9d589711debad283427697
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-21 09:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (30.65%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,33 +1,41 @@
-// class Solution {
-// public:
-//     int specialArray(vector<int>& nums) {
-//         int n = nums.size();
-//         sort(nums.begin(), nums.end());
-//         int l = 0, h = 1000;
-
-//         while(l <= h){
-//             int mid = l + (h-l)/2;
-//             for(int& num: nums){
-
-//             }
-//         }
-//     }
-// };
-
-
-//       **************** Brute Force : Linear seraching **************
-
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
         int n = nums.size();
-        for(int i=0; i<=n; i++){
-            int count = 0;
-            for(int& num : nums){
-                if(num >= i)count++;
+        sort(nums.begin(), nums.end());
+        int l = 0, h = n;
+
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            int i = lower_bound(nums.begin(), nums.end(), mid)- nums.begin();
+            if(n-i > mid){
+                l = mid+1;
             }
-            if(count == i) return i;
+            else if(n-i < mid){
+                h = mid-1;
+            }
+            else{
+                return mid;
+            }
         }
         return -1;
     }
 };
+
+
+//       **************** Brute Force **************
+
+// class Solution {
+// public:
+//     int specialArray(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i=0; i<=n; i++){
+//             int count = 0;
+//             for(int& num : nums){
+//                 if(num >= i)count++;
+//             }
+//             if(count == i) return i;
+//         }
+//         return -1;
+//     }
+// };

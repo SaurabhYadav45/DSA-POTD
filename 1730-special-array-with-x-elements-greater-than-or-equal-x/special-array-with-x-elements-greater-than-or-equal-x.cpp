4b11d202941d948fe5a6eab8020d06dc9e932e76
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-07-21 09:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (88.46%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,8 +1,27 @@
+// class Solution {
+// public:
+//     int specialArray(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+//         int l = 0, h = 1000;
+
+//         while(l <= h){
+//             int mid = l + (h-l)/2;
+//             for(int& num: nums){
+
+//             }
+//         }
+//     }
+// };
+
+
+//       **************** Brute Force : Linear seraching **************
+
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<=1000; i++){
+        int n = nums.size();
+        for(int i=0; i<=n; i++){
             int count = 0;
             for(int& num : nums){
                 if(num >= i)count++;

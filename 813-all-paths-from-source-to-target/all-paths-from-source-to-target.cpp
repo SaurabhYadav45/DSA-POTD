0676f6dbefd72f8093daf5dec4d383bd9203e6c0
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-04 09:03:08 +0530

    Time: 15 ms (26.32%) | Memory: 19.9 MB (18.54%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
-    int n;
-    void dfs(int src, vector<vector<int>>& graph, vector<int>&curr, vector<vector<int>>&result){
-        curr.push_back(src);
-
-        if(src == n-1){
-            result.push_back(curr);
-        }
-
-        for(auto& nbr: graph[src]){
-            dfs(nbr, graph, curr, result);
-        }
-
-        curr.pop_back(); // Backtrack
-    }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        n = graph.size();
+        int n = graph.size();
         vector<vector<int>>result;
-        vector<int>curr;
-        dfs(0, graph, curr, result);
+        queue<vector<int>>q;
+
+        q.push({0});
+
+        while(!q.empty()){
+            vector<int>curr = q.front();
+            int lastNode = curr.back();
+            q.pop();
+
+            if(lastNode == n-1){
+                result.push_back(curr);
+            }
+
+            for(auto& nbr: graph[lastNode]){
+                curr.push_back(nbr);
+                q.push(curr);
+                curr.pop_back();
+            }
+        }
         return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     int n;
+//     void dfs(int src, vector<vector<int>>& graph, vector<int>&curr, vector<vector<int>>&result){
+//         curr.push_back(src);
+
+//         if(src == n-1){
+//             result.push_back(curr);
+//         }
+
+//         for(auto& nbr: graph[src]){
+//             dfs(nbr, graph, curr, result);
+//         }
+
+//         curr.pop_back(); // Backtrack
+//     }
+//     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+//         n = graph.size();
+//         vector<vector<int>>result;
+//         vector<int>curr;
+//         dfs(0, graph, curr, result);
+//         return result;
+//     }
+// };

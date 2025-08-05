e859b7dfd11fcbc55593b6e0a55d33394072964a
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-05 11:29:23 +0530

    Time: 6 ms (71.46%) | Memory: 238 MB (95.14%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -5,31 +5,36 @@ public:
         int n = waterStartTime.size();
 
         int result = INT_MAX;
-        int landStartMinimum = INT_MAX;
-        int waterStartMinimum = INT_MAX;
+        int landRideMinimum = INT_MAX;
+        int waterRideMinimum = INT_MAX;
 
+        // find the minimum time taken Land Ride
         for(int i=0; i<m; i++){
             int time  = landStartTime[i] + landDuration[i];
-            landStartMinimum = min(landStartMinimum, time);
+            landRideMinimum = min(landRideMinimum, time);
         }
 
+        // find the minimum time taken Water Ride
         for(int i=0; i<n; i++){
             int time  = waterStartTime[i] + waterDuration[i];
-            waterStartMinimum = min(waterStartMinimum, time);
+            waterRideMinimum = min(waterRideMinimum, time);
         }
 
+        // Case1: Calculate Minimum time taken  (LAND -> WATER) Ride
         for(int j=0; j<n; j++){
-            int landEnd = max(landStartMinimum, waterStartTime[j]);
+            // Wait for Water ride to start (take the maximum time )
+            int landEnd = max(landRideMinimum, waterStartTime[j]); 
             int waterDur = waterDuration[j];
             result = min(result, landEnd + waterDur);
         }
 
+        // Case2: Calculate Minimum time taken  (WATER -> LAND) Ride
         for(int j=0; j<m; j++){
-            int waterEnd = max(waterStartMinimum, landStartTime[j]);
+            // Wait for Land ride to start
+            int waterEnd = max(waterRideMinimum, landStartTime[j]);
             int landDur = landDuration[j];
             result = min(result, waterEnd + landDur);
         }
-
         return result;
     }
 };

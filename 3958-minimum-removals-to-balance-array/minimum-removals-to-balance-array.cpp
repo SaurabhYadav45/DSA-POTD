93ea7fb7617389f2ec01521dd19dea9f4f3b7cae
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-05 10:09:35 +0530

    Time: 45 ms (33.30%) | Memory: 105 MB (86.96%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -1,25 +1,66 @@
 //        **************** Sliding window **************
 
+// class Solution {
+// public:   
+//     int minRemoval(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+
+//         int left = 0;
+//         int right = 0;
+//         int maxLength = 0;
+
+//         while(right < n){
+//             while(nums[right] > 1LL* nums[left]*k){
+//                 left++;
+//             }
+//             maxLength = max(maxLength, right-left+1);
+//             right++;
+//         }
+//         return n-maxLength;
+//     }
+// };
+
+
+//     **************** Binary Search ******************
+
 class Solution {
-public:   
+public:
+    bool isPossible(vector<int>& nums, int k, int mid) {
+        int n = nums.size();
+        for (int i = 0; i <= mid; i++) {
+            int start = i;                  // Remove i from start
+            int end = n - (mid - i) - 1;    // Remove (mid-i) from end
+
+            int mini = nums[start];
+            int maxi = nums[end];
+
+            if (1LL * maxi <= 1LL * mini * k) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     int minRemoval(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        int left = 0;
-        int right = 0;
-        int maxLength = 0;
+        int low = 0, high = n - 1, result = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-        while(right < n){
-            while(nums[right] > 1LL* nums[left]*k){
-                left++;
+            if (isPossible(nums, k, mid)) {
+                result = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1; 
             }
-            maxLength = max(maxLength, right-left+1);
-            right++;
         }
-        return n-maxLength;
+        return result;
     }
 };
+
 
 
 

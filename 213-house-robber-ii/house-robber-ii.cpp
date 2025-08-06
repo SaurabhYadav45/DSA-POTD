3df86ed5cafb039c044f52b8a7264233829b0c28
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-06 12:11:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (89.25%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -5,29 +5,65 @@ public:
     if(n == 1) return nums[0];
     if(n == 2) return max(nums[0], nums[1]);
 
-    vector<int>dp(n+1, 0);
-    dp[0] = 0;
-    for(int i=1; i<=n-1; i++){
-        int steal = nums[i-1] + ((i-2 >= 0) ? dp[i-2] : 0);
-        int skip = dp[i-1];
-        dp[i] = max(steal, skip);
+    int prev = 0;
+    int prevNext = nums[0];
+    int curr = 0;
+    for(int i=1; i<=n-2; i++){
+        curr = max(nums[i]+prev, prevNext);
+        prev = prevNext;
+        prevNext = curr;
     }
-    int result1 = dp[n-1];
+    int result1 = curr;
 
-    dp.assign(n+1, 0);
-    dp[0] = 0;
-    dp[1] = 0;
-    for(int i=2; i<=n; i++){
-        int steal = nums[i-1] + ((i-2 >= 0) ? dp[i-2] : 0);
-        int skip = dp[i-1];
-        dp[i] = max(steal, skip);
+    prev = 0;
+    prevNext = nums[1];
+    curr = 0;
+    for(int i=2; i<=n-1; i++){
+        curr = max(nums[i]+prev, prevNext);
+        prev = prevNext;
+        prevNext = curr;
     }
-    int result2 = dp[n];
+    int result2 = curr;
 
     int maxAmt = max(result1, result2);
     return maxAmt;
     }
 };
+
+
+
+//       ************* Bottom Up **************
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//     int n = nums.size();
+//     if(n == 1) return nums[0];
+//     if(n == 2) return max(nums[0], nums[1]);
+
+//     vector<int>dp(n+1, 0);
+//     dp[0] = 0;
+//     for(int i=1; i<=n-1; i++){
+//         int steal = nums[i-1] + ((i-2 >= 0) ? dp[i-2] : 0);
+//         int skip = dp[i-1];
+//         dp[i] = max(steal, skip);
+//     }
+//     int result1 = dp[n-1];
+
+//     dp.assign(n+1, 0);
+//     dp[0] = 0;
+//     dp[1] = 0;
+//     for(int i=2; i<=n; i++){
+//         int steal = nums[i-1] + ((i-2 >= 0) ? dp[i-2] : 0);
+//         int skip = dp[i-1];
+//         dp[i] = max(steal, skip);
+//     }
+//     int result2 = dp[n];
+
+//     int maxAmt = max(result1, result2);
+//     return maxAmt;
+//     }
+// };
 
 
 

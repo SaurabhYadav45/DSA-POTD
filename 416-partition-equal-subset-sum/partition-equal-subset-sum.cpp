001e7a6d87e4ad6b5f2f486e80c0de2f0d7cd7ab
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-07 14:44:58 +0530

    Time: 172 ms (21.14%) | Memory: 17.5 MB (55.21%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -15,14 +15,22 @@ public:
 
         vector<vector<bool>>dp(n+1, vector<bool>(target+1, false));
 
+        // for(int i = 0; i < n+1; i++) {
+        //     for(int j = 0; j < target+1; j++) {
+        //         if(j == 0)
+        //             dp[i][j] = true;  // sum = 0 is always possible (empty subset)
+        //         else if(i == 0)
+        //             dp[i][j] = false; // non-zero sum with 0 items not possible
+        //     }
+        // }
+
         for(int i = 0; i < n+1; i++) {
-            for(int j = 0; j < target+1; j++) {
-                if(j == 0)
-                    dp[i][j] = true;  // sum = 0 is always possible (empty subset)
-                else if(i == 0)
-                    dp[i][j] = false; // non-zero sum with 0 items not possible
-            }
+            dp[i][0] = true;
         }
+
+        // for(int i = 0; i < target+1; i++) {
+        //     dp[0][i] = false;
+        // }
 
         for(int i = 1; i < n+1; i++) {
             for(int j = 1; j < target+1; j++) {

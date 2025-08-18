44282ee0d71eb8bfdb7928e8140a54dedabf01a6
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-18 23:43:05 +0530

    Time: 63 ms (20.48%) | Memory: 30.6 MB (18.28%) - LeetSync

--- 679-24-game/24-game.cpp
+++ 679-24-game/24-game.cpp
@@ -1,46 +1,48 @@
 class Solution {
-    const double EPS = 1e-6;
-
 public:
-    bool judgePoint24(vector<int>& cards) {
-        vector<double> nums;
-        for (int n : cards) nums.push_back((double)n);
-        return dfs(nums);
-    }
-
-private:
-    bool dfs(vector<double>& nums) {
-        if (nums.size() == 1) {
-            return fabs(nums[0] - 24.0) < EPS;
+    double epsilon = 0.1;
+    bool solve(vector<double>& nums){
+        if(nums.size() == 1){
+            return abs(24 - nums[0]) <= epsilon;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                if (i == j) continue;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                if(i == j) continue;
 
-                vector<double> next;
-                for (int k = 0; k < nums.size(); k++) {
-                    if (k != i && k != j) next.push_back(nums[k]);
+                vector<double>rem;
+                for(int k=0; k<nums.size(); k++){
+                    if(k != i && k != j){
+                        rem.push_back(nums[k]);
+                    }
                 }
 
-                for (double val : compute(nums[i], nums[j])) {
-                    next.push_back(val);
-                    if (dfs(next)) return true;
-                    next.pop_back();
+                double a = nums[i];
+                double b = nums[j];
+                vector<double>possible = {a+b, a-b, b-a, a*b};
+                if(b != 0.0){
+                    possible.push_back(a/b);
+                }
+                if(a != 0.0){
+                    possible.push_back(b/a);
+                }
+
+                for(double& num: possible){
+                    rem.push_back(num);
+                    if(solve(rem)){
+                        return true;
+                    }
+                    rem.pop_back();
                 }
             }
         }
         return false;
     }
-
-    vector<double> compute(double a, double b) {
-        vector<double> res;
-        res.push_back(a + b);
-        res.push_back(a - b);
-        res.push_back(b - a);
-        res.push_back(a * b);
-        if (fabs(b) > EPS) res.push_back(a / b);
-        if (fabs(a) > EPS) res.push_back(b / a);
-        return res;
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> nums(cards.size(), 0);
+        for(int i=0; i<cards.size(); i++){
+            nums[i] = (1.0 * cards[i]);
+        }
+        return solve(nums);
     }
 };

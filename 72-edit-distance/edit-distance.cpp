cae5d18017d3a27612fdd267015e1a13a24a7160
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-19 11:53:13 +0530

    Time: 11 ms (29.89%) | Memory: 13.2 MB (47.26%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,31 +1,62 @@
 class Solution {
 public:
     int m, n;
-    int solve(int i, int j, string& word1, string& word2, vector<vector<int>>&dp){
-        if(i == 0 || j == 0){
-            return i+j;
+    int solve(int i, int j, string& s1, string& s2){
+        vector<vector<int>>dp(m+1, vector<int>(n+1, 0));
+        // dp[i][j] = Minimum no. of operations required for making s1 of length i & s2 of length j equal
+        for(int i=0; i<m+1; i++){
+            for(int j=0; j<n+1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = i + j;
+                }
+                else if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+                }
+            }
         }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if(word1[i-1] == word2[j-1]){
-            return solve(i-1, j-1, word1, word2, dp);
-        }
-
-        int insert = 1 + solve(i, j-1, word1, word2, dp);
-        int deletes = 1 + solve(i-1, j, word1, word2, dp);
-        int replace = 1 + solve(i-1, j-1, word1, word2, dp);
-        return dp[i][j] = min({insert, deletes, replace});
+        return dp[m][n];
     }
     int minDistance(string word1, string word2) {
         m = word1.length();
         n = word2.length();
-        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
-        return solve(m, n, word1, word2, dp);
+        return solve(m, n, word1, word2);
     }
 };
+
+
+//        ******************* Rec + Memo *******************
+
+// class Solution {
+// public:
+//     int m, n;
+//     int solve(int i, int j, string& word1, string& word2, vector<vector<int>>&dp){
+//         if(i == 0 || j == 0){
+//             return i+j;
+//         }
+
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+
+//         if(word1[i-1] == word2[j-1]){
+//             return solve(i-1, j-1, word1, word2, dp);
+//         }
+
+//         int insert = 1 + solve(i, j-1, word1, word2, dp);
+//         int deletes = 1 + solve(i-1, j, word1, word2, dp);
+//         int replace = 1 + solve(i-1, j-1, word1, word2, dp);
+//         return dp[i][j] = min({insert, deletes, replace});
+//     }
+//     int minDistance(string word1, string word2) {
+//         m = word1.length();
+//         n = word2.length();
+//         vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+//         return solve(m, n, word1, word2, dp);
+//     }
+// };
 
 //     int solveUsingRec(string &word1, string &word2, int i, int j){
 //         int ans = 0;

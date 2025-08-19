6e7e725a0b2a0391534f5e27d5eaa6f1ca907137
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-19 17:48:26 +0530

    Time: 18 ms (91.58%) | Memory: 31.3 MB (75.25%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:   
-    int solve(int i, int j, string& s, vector<vector<int>>&dp){
-        if(i >= j) return 0;
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        if(s[i] == s[j]){
-            return dp[i][j] = solve(i+1, j-1, s, dp);
-        }
-        else{
-            int case1 = solve(i+1, j, s, dp);
-            int case2 = solve(i, j-1, s, dp);
-            return dp[i][j] = 1 + min(case1, case2);
-        }
-    }
     int minInsertions(string s) {
         int n = s.length();
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return solve(0, n-1, s, dp);
+        vector<vector<int>>dp(n, vector<int>(n));
+
+        // dp[i][j] = 
+
+        for(int i=0; i<n; i++){
+            dp[i][i] = 0;   // string of length one already a palindrome
+        }
+
+        for(int l=2; l<n+1; l++){
+            for(int i=0; i+l-1< n; i++){
+                int j = i+l-1;
+                if(s[i] == s[j]){
+                    dp[i][j] = dp[i+1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 };
+
+
+// class Solution {
+// public:   
+//     int solve(int i, int j, string& s, vector<vector<int>>&dp){
+//         if(i >= j) return 0;
+
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+        
+//         if(s[i] == s[j]){
+//             return dp[i][j] = solve(i+1, j-1, s, dp);
+//         }
+//         else{
+//             int case1 = solve(i+1, j, s, dp);
+//             int case2 = solve(i, j-1, s, dp);
+//             return dp[i][j] = 1 + min(case1, case2);
+//         }
+//     }
+//     int minInsertions(string s) {
+//         int n = s.length();
+//         vector<vector<int>>dp(n, vector<int>(n, -1));
+//         return solve(0, n-1, s, dp);
+//     }
+// };
 
 
 

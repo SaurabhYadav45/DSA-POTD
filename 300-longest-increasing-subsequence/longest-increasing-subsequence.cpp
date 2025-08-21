172cf4feff35e5bb4441337a521b81ebd1899a8f
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-21 15:49:58 +0530

    Time: 79 ms (52.48%) | Memory: 14.3 MB (73.79%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:  
-    int solve(int i, int p, vector<int>& nums, vector<vector<int>>&dp){
-        if(i >= nums.size()){
-            return 0;
-        }
-
-        if(p != -1 && dp[i][p] != -1){
-            return dp[i][p];
-        }
-
-        int take = 0;
-        if(p == -1 || nums[i] > nums[p]){
-            take = 1 + solve(i+1, i, nums, dp);
-        }
-
-        int skip = solve(i+1, p, nums, dp);
-        int ans = max(take, skip);
-        if(p != -1){
-            dp[i][p] = ans;
-        }
-        return ans;
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return solve(0, -1, nums, dp);
+        vector<int>dp(n, 1);
+        int maxLIS = 1;
+        // State Definition :  dp[i] = LIS in nums[0...i];
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                    maxLIS = max(dp[i], maxLIS);
+                }
+            }
+        }
+        return maxLIS;
     }
 };
+
+// class Solution {
+// public:  
+//     int solve(int i, int prev, vector<int>& nums, vector<vector<int>>&dp){
+//         if(i >= nums.size()){
+//             return 0;
+//         }
+
+//         if(dp[i][prev+1] != -1){
+//             return dp[i][prev+1];
+//         }
+
+//         int take = 0;
+//         if(prev == -1 || nums[i] > nums[prev]){
+//             take = 1 + solve(i+1, i, nums, dp);
+//         }
+
+//         int skip = solve(i+1, prev, nums, dp);
+//         return dp[i][prev+1] = max(take, skip);
+//     }
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>>dp(n, vector<int>(n+1, -1));
+//         return solve(0, -1, nums, dp);
+//     }
+// };
 
 
 // class Solution {

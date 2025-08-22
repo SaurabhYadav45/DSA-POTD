a1d57ac100aa90801902d9452b7cc892307eb8dd
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-22 13:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (99.23%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,42 @@
+//         **************** Binary Search : Patience Sort *********
 class Solution {
 public:  
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, 1);
-        int maxLIS = 1;
-        // State Definition :  dp[i] = LIS in nums[0...i];
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                    maxLIS = max(dp[i], maxLIS);
-                }
+        vector<int>sorted;
+        for(int i=0; i<n; i++){
+            auto it = lower_bound(sorted.begin(), sorted.end(), nums[i]);
+            if(it == sorted.end()){
+                sorted.push_back(nums[i]);
+            }
+            else{
+                *it = nums[i];
             }
         }
-        return maxLIS;
+        return (int)sorted.size();
     }
 };
+
+//       ************** Bottom Up **************
+
+// class Solution {
+// public:  
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>dp(n, 1);
+//         int maxLIS = 1;
+//         // State Definition :  dp[i] = LIS in nums[0...i];
+//         for(int i=1; i<n; i++){
+//             for(int j=0; j<i; j++){
+//                 if(nums[j] < nums[i]){
+//                     dp[i] = max(dp[i], 1 + dp[j]);
+//                     maxLIS = max(dp[i], maxLIS);
+//                 }
+//             }
+//         }
+//         return maxLIS;
+//     }
+// };
 
 // class Solution {
 // public:  
@@ -40,57 +61,5 @@ public:
 //         int n = nums.size();
 //         vector<vector<int>>dp(n, vector<int>(n+1, -1));
 //         return solve(0, -1, nums, dp);
-//     }
-// };
-
-
-// class Solution {
-// public:
-
-//     int solveUsingRecAndMemo(vector<int>& nums, int i, int p, vector<vector<int>>&dp){
-//         // base case
-//         if(i >= nums.size()) return 0;
-
-//         if( p != -1 && dp[i][p] != -1 ){
-//              return dp[i][p];
-//         }
-
-//         int skip  = solveUsingRecAndMemo(nums, i+1, p, dp);
-//         int take = 0;
-//         if(p == -1 || nums[p] < nums[i]){
-//              take = 1 + solveUsingRecAndMemo(nums, i+1, i, dp);
-//         }
-
-//         int ans  = max(take, skip);
-//         if(p != -1){
-//             dp[i][p] = ans;
-//         }
-        
-//         return ans ;
-//     }
-
-
-//     int solveUsingTabulation(vector<int>& nums){
-//         int n = nums.size();
-//         vector<int>dp(n+1, 1);
-//         int maxLIS = 1;
-//         for(int i=0; i< n; i++){
-//             for(int j=0; j<i ; j++){
-//                 if(nums[j]  < nums[i]){
-//                     dp[i] = max(dp[i], dp[j]+1);
-//                     maxLIS = max(dp[i], maxLIS);
-//                 }
-//             }
-//         }
-//         return maxLIS;
-//     }
-
-    
-//     int lengthOfLIS(vector<int>& nums) {
-//         int n = nums.size();
-//         // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-//         // int ans = solveUsingRecAndMemo(nums, 0, -1, dp);
-//         int ans = solveUsingTabulation(nums);
-//         return ans;
 //     }
 // };

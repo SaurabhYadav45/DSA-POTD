30a9658108f03e8111a944bc987bec014a996981
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-25 16:18:38 +0530

    Time: 25 ms (30.10%) | Memory: 31.2 MB (23.15%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,39 @@
+// class Solution {
+// public:
+//     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+       
+//     }
+// };
+
+
+
+
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+       int m = mat.size();
+       int n = mat[0].size();
+       map<int, vector<int>>mp;
+       vector<int>result;
+
+       for(int i=0;i<m; i++){
+        for(int j=0; j<n; j++){
+            mp[i+j].push_back(mat[i][j]);
+        }
+       } 
+
+       bool flag = true;
+       for(auto& it : mp){
+            if(flag){
+                reverse(it.second.begin(), it.second.end());
+            }
+
+            for(int& num: it.second){
+                result.push_back(num);
+            }
+
+            flag = !flag;
+       }
+       return result;
+    }
+};

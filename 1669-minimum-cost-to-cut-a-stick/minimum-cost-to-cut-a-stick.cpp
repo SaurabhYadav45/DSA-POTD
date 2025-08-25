1ff863d89ac9f08399e2b3c2dc6bf565ed189182
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-25 12:38:44 +0530

    Time: 42 ms (11.92%) | Memory: 14 MB (51.21%) - LeetSync

---
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int solve(int start, int end, vector<int>& cuts, vector<vector<int>>&dp){
+        if(end-start < 2){
+            return 0;
+        }
+
+        if(dp[start][end] != -1){
+            return dp[start][end];
+        }
+
+        int result = INT_MAX;
+        for(int i=start+1; i <= end-1; i++){
+            int cost = cuts[end]-cuts[start];
+            int left = solve(start, i, cuts, dp);
+            int right = solve(i, end, cuts, dp);
+            result = min(result, left+right+cost);
+        }
+        return dp[start][end] = result;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(), cuts.end());
+        cuts.insert(cuts.begin() + 0, 0);
+        cuts.push_back(n);
+        vector<vector<int>>dp(cuts.size()+1, vector<int>(cuts.size()+1, -1));
+        return solve(0, cuts.size()-1, cuts, dp);
+    }
+};
+
+
+// class Solution {
+// public:
+//     int solve(int start, int end, vector<int>& cuts, vector<vector<int>>&dp) {
+
+//         if(dp[start][end] != -1){
+//             return dp[start][end];
+//         }
+
+//         int cost = INT_MAX;
+//         bool canCut = false;
+
+//         for(int& c: cuts){
+//             if(c > start && c < end){
+//                 canCut = true;
+//                 int left = solve(start, c, cuts, dp);
+//                 int right = solve(c, end, cuts, dp);
+//                 cost = min(cost, left + right + (end-start));
+//             }
+//         }
+//         return dp[start][end] = canCut ? cost : 0;
+//     }
+//     int minCost(int n, vector<int>& cuts) {
+//         vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+//         return solve(0, n, cuts, dp);
+//     }
+// };

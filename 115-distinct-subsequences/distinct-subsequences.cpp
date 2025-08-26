e6d02b320c0cdc509ac04d4f40e4c1e9afd1d1a5
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-26 10:46:03 +0530

    Time: 31 ms (54.93%) | Memory: 26.9 MB (62.91%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int solve(int i, int j, string& s, string& t, vector<vector<int>>&dp){
+        if(j == t.size()) return 1; 
+        if(i == s.size()) return 0;
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int take = 0;
+        if(s[i] == t[j]){
+            take = solve(i+1, j+1, s, t, dp);
+        }
+
+        int skip = solve(i+1, j, s, t, dp);
+        long long ways = take + skip;
+        return dp[i][j] = ways;
+    }
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        if(n > m) return 0;
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return solve(0, 0, s, t, dp);
+    }
+};
+
+
+// class Solution {
+// public:
+//     int solve(int i, string&curr, string& s, string& t){
+//         if(i >= s.size()){
+//             if(curr == t) return 1;
+//             return 0;
+//         }
+//         // take
+//         curr.push_back(s[i]);
+//         int take = solve(i+1, curr, s, t);
+//         curr.pop_back(); // backtrack
+//         // skip
+//         int skip = solve(i+1, curr, s, t);
+
+//         return take + skip;
+//     }
+//     int numDistinct(string s, string t) {
+//         int n = s.length();
+//         string curr = "";
+//         return solve(0, curr, s, t);
+//     }
+// };

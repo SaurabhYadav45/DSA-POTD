044d50d602fe138ac125756af854ea97c8a56c70
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-26 10:09:40 +0530

    Time: 25 ms (10.31%) | Memory: 18.2 MB (8.03%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,5 +1,38 @@
+// class Solution {
+// public:
+//     int minDistance(string word1, string word2) {
+//         int m = word1.size();
+//         int n = word2.size();
+//         vector<vector<int>>dp(m+1, vector<int>(n+1, 0));
+//         // Initialization
+//         for(int i=0; i<m+1; i++){
+//             for(int j=0; j<n+1; j++){
+//                 if(i == 0 || j == 0){
+//                     dp[i][j] = i+j;
+//                 }
+//             }
+//         }
+//         // Fill the table
+//         for(int i=1; i<m+1; i++){
+//             for(int j=1; j<n+1; j++){
+//                 if(word1[i-1] == word2[j-1]){
+//                     dp[i][j] = dp[i-1][j-1]; // Match
+//                 }
+//                 else{ // Didn't match
+//                     int case1 = 1 + dp[i-1][j];
+//                     int case2 = 1 + dp[i][j-1];
+//                     dp[i][j] = min({case1, case2});
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
+
+
 class Solution {
 public:
+    int m, n;
     int solve(int i, int j, string& s1, string& s2, vector<vector<int>>&dp){
         if(i == 0 || j == 0){
             return i + j;
@@ -9,70 +42,23 @@ public:
             return dp[i][j];
         }
         
-        int case1 = 1e9;
         if(s1[i-1] == s2[j-1]){
-            case1 = solve(i-1, j-1, s1, s2, dp);
+            dp[i][j] = solve(i-1, j-1, s1, s2, dp);
         }
-        int case2 = 1 + solve(i-1, j, s1, s2, dp);
-        int case3 = 1 + solve(i, j-1, s1, s2, dp);
-        return dp[i][j] = min({case1, case2, case3});
+        else{
+            int case1 = 1 + solve(i-1, j, s1, s2, dp);
+            int case2 = 1 + solve(i, j-1, s1, s2, dp);
+            dp[i][j] = min({case1, case2});
+        }
+        return dp[i][j];
     }
     int minDistance(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
-        vector<vector<int>>dp(m+1, vector<int>(n+1, 0));
-
-        for(int i=0; i<m+1; i++){
-            for(int j=0; j<n+1; j++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = i+j;
-                }
-            }
-        }
-        
-        for(int i=1; i<m+1; i++){
-            for(int j=1; j<n+1; j++){
-                int case1 = 1e9;
-                if(word1[i-1] == word2[j-1]){
-                    case1 = dp[i-1][j-1];
-                }
-                int case2 = 1 + dp[i-1][j];
-                int case3 = 1 + dp[i][j-1];
-                dp[i][j] = min({case1, case2, case3});
-            }
-        }
-        return dp[m][n];
+        m = word1.size();
+        n = word2.size();
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        return solve(m, n, word1, word2, dp);
     }
 };
-
-
-// class Solution {
-// public:
-//     int m, n;
-//     int solve(int i, int j, string& s1, string& s2, vector<vector<int>>&dp){
-//         if(i == 0 || j == 0){
-//             return i + j;
-//         }
- 
-//         if(dp[i][j] != -1){
-//             return dp[i][j];
-//         }
-        
-//         int case1 = 1e9;
-//         if(s1[i-1] == s2[j-1]){
-//             case1 = solve(i-1, j-1, s1, s2, dp);
-//         }
-//         int case2 = 1 + solve(i-1, j, s1, s2, dp);
-//         int case3 = 1 + solve(i, j-1, s1, s2, dp);
-//         return dp[i][j] = min({case1, case2, case3});
-//     }
-//     int minDistance(string word1, string word2) {
-//         m = word1.size();
-//         n = word2.size();
-//         vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
-//         return solve(m, n, word1, word2, dp);
-//     }
-// };
 
 
 

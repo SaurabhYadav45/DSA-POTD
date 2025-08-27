2d22e55c2947fe9694cdc020e5f0702bdec0869d
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-27 20:44:17 +0530

    Time: 1654 ms (28.34%) | Memory: 95.1 MB (92.51%) - LeetSync

---
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>>directions{{1,1}, {1,-1}, {-1,-1}, {-1,1}};
+    int solve(int i, int j, int d, bool canTurn, int val, vector<vector<int>>& grid){
+        int x = i + directions[d][0];
+        int y = j + directions[d][1];
+
+        if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != val){
+            return 0;
+        }
+
+        int length = 0;
+
+        int keepMoving = 1 + solve(x, y, d, canTurn, (val==2?0:2), grid);
+        length = max(length, keepMoving);
+
+        if(canTurn){
+            int takeTurn = 1 + solve(x, y, (d+1)%4, false, (val==2?0:2), grid);
+            length = max(length, takeTurn);
+        }
+        return length;
+    }
+    int lenOfVDiagonal(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        int result = 0;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    for(int d=0; d<=3; d++){
+                        result =  max(result, 1+solve(i, j, d, true, 2, grid));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};

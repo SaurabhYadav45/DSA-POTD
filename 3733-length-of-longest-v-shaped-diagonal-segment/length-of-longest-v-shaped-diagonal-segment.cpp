680b4a75068d43617ed09ee87211e884b5fda58d
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-27 20:55:14 +0530

    Time: 2081 ms (20.85%) | Memory: 104 MB (67.91%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int m, n;
+    int memo[501][501][4][2];
     vector<vector<int>>directions{{1,1}, {1,-1}, {-1,-1}, {-1,1}};
     int solve(int i, int j, int d, bool canTurn, int val, vector<vector<int>>& grid){
         int x = i + directions[d][0];
@@ -8,6 +9,10 @@ public:
 
         if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != val){
             return 0;
+        }
+
+        if(memo[x][y][d][canTurn] != -1){
+            return memo[x][y][d][canTurn];
         }
 
         int length = 0;
@@ -25,6 +30,8 @@ public:
         m = grid.size();
         n = grid[0].size();
         int result = 0;
+
+        memset(memo, -1, sizeof(memo));
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){

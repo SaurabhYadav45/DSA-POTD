62bf9301e572b85637265ba5ebf92f11286eccb0
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-08-30 11:57:17 +0530

    Time: 19 ms (80.34%) | Memory: 14.1 MB (25.68%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
-    int solve(int start, int end, vector<int>& cuts, vector<vector<int>>&dp){
-        if(end-start < 2){
-            return 0;
-        }
-
-        if(dp[start][end] != -1){
-            return dp[start][end];
-        }
-
-        int result = INT_MAX;
-        for(int i=start+1; i <= end-1; i++){
-            int cost = cuts[end]-cuts[start];
-            int left = solve(start, i, cuts, dp);
-            int right = solve(i, end, cuts, dp);
-            result = min(result, left+right+cost);
-        }
-        return dp[start][end] = result;
-    }
     int minCost(int n, vector<int>& cuts) {
         sort(cuts.begin(), cuts.end());
         cuts.insert(cuts.begin() + 0, 0);
         cuts.push_back(n);
-        vector<vector<int>>dp(cuts.size()+1, vector<int>(cuts.size()+1, -1));
-        return solve(0, cuts.size()-1, cuts, dp);
+        int m = cuts.size();
+        vector<vector<int>>dp(cuts.size(), vector<int>(cuts.size(), 0));
+        // State Definition :  dp[i][j] Minimum cost to cut stick b/w i and j
+
+        // Initialization
+        // dp[i][i] = 0 for all i < n
+        
+        // Fill the table
+        for(int length = 2; length<m; length++){
+            for(int i=0; i+length<m; i++){
+                int j = i + length;
+                dp[i][j] = INT_MAX;
+                for(int k=i+1; k <j; k++){
+                    int cost = (cuts[j]-cuts[i]) + dp[i][k] + dp[k][j];
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[0][m-1];
     }
 };
+
+
+// class Solution {
+// public:
+//     int solve(int start, int end, vector<int>& cuts, vector<vector<int>>&dp){
+//         if(end-start < 2){
+//             return 0;
+//         }
+
+//         if(dp[start][end] != -1){
+//             return dp[start][end];
+//         }
+
+//         int result = INT_MAX;
+//         for(int i=start+1; i <= end-1; i++){
+//             int cost = cuts[end]-cuts[start];
+//             int left = solve(start, i, cuts, dp);
+//             int right = solve(i, end, cuts, dp);
+//             result = min(result, left+right+cost);
+//         }
+//         return dp[start][end] = result;
+//     }
+//     int minCost(int n, vector<int>& cuts) {
+//         sort(cuts.begin(), cuts.end());
+//         cuts.insert(cuts.begin() + 0, 0);
+//         cuts.push_back(n);
+//         vector<vector<int>>dp(cuts.size()+1, vector<int>(cuts.size()+1, -1));
+//         return solve(0, cuts.size()-1, cuts, dp);
+//     }
+// };
 
 
 // class Solution {

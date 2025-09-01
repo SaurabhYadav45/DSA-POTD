abc7f39ed5c1bad9946879cb055106f49c9aa407
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-09-01 11:08:58 +0530

    Time: 7 ms (3.75%) | Memory: 50.1 MB (5.30%) - LeetSync

---
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int maxSum = 0;
+        vector<int>pSum(n+1, 0);
+        vector<int>sSum(n+1, 0);
+        // pSum[0] = cardPoints[0];
+        
+        for(int i=0; i<n; i++){
+            pSum[i+1] = pSum[i] + cardPoints[i];
+        }
+
+        // sSum[n-1] = cardPoints[n-1];
+        for(int i=n-1; i>=0; i--){
+            sSum[i] = sSum[i+1] + cardPoints[i];
+        }
+
+        for(int i=0; i<=k; i++){
+            int sum = pSum[i] + sSum[n-k+i];
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
+};
+
+//     ***************** Brute Force : TLE ****************
+
+// class Solution {
+// public:
+//     int maxScore(vector<int>& cardPoints, int k) {
+//         int n = cardPoints.size();
+//         int maxSum = 0;
+//         for(int i=0; i<=k; i++){
+//             int sum = 0;
+//             for(int j=0; j<i; j++){
+//                 sum += cardPoints[j];
+//             }
+
+//             for(int j=n-k+i; j < n; j++){
+//                 sum += cardPoints[j];
+//             }
+
+//             maxSum = max(maxSum, sum);
+//         }
+//         return maxSum;
+//     }
+// };

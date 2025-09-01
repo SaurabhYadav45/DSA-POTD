a1c5391274134e4d99266fd2b64c0b755eacae81
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-09-01 13:14:54 +0530

    Time: 44 ms (38.12%) | Memory: 75.1 MB (54.28%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -3,24 +3,52 @@ public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
         unordered_map<int, int>mp;
-
         int i=0, j=0;
         int result = 0;
 
         while(j < n){
             int fruit = fruits[j];
             mp[fruit]++;
-            while(mp.size() > 2){
+            if(mp.size() <= 2){
+                result = max(result, j-i+1);
+            }
+            else{
                 mp[fruits[i]]--;
                 if(mp[fruits[i]] == 0){
                     mp.erase(fruits[i]);
                 }
                 i++;
             }
-
-            result = max(result, j-i+1);
             j++;
         }
         return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
+//         int n = fruits.size();
+//         unordered_map<int, int>mp;
+
+//         int i=0, j=0;
+//         int result = 0;
+
+//         while(j < n){
+//             int fruit = fruits[j];
+//             mp[fruit]++;
+//             while(mp.size() > 2){
+//                 mp[fruits[i]]--;
+//                 if(mp[fruits[i]] == 0){
+//                     mp.erase(fruits[i]);
+//                 }
+//                 i++;
+//             }
+
+//             result = max(result, j-i+1);
+//             j++;
+//         }
+//         return result;
+//     }
+// };

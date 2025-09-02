164fa5709655e04556c48f2d7c95003aed2799ca
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-09-02 09:32:27 +0530

    Time: 43 ms (8.59%) | Memory: 16.8 MB (8.25%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -3,16 +3,17 @@ public:
     int numberOfSubstrings(string s) {
         int n = s.length();
         int count = 0;
-        vector<int>freq(3, 0);
-        int i=0;
-        int j=0;
-        
+        unordered_map<char, int>mp;
+        int i=0, j=0;
+
         while(j < n){
-            char ch = s[j];
-            freq[ch-'a']++;
-            while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0){
+            mp[s[j]]++;
+            while(mp.size() >= 3){
                 count += n-j;
-                freq[s[i] -'a']--;
+                mp[s[i]]--;
+                if(mp[s[i]] == 0){
+                    mp.erase(s[i]);
+                }
                 i++;
             }
             j++;
@@ -20,6 +21,30 @@ public:
         return count;
     }
 };
+
+
+// class Solution {
+// public:
+//     int numberOfSubstrings(string s) {
+//         int n = s.length();
+//         int count = 0;
+//         vector<int>freq(3, 0);
+//         int i=0;
+//         int j=0;
+        
+//         while(j < n){
+//             char ch = s[j];
+//             freq[ch-'a']++;
+//             while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0){
+//                 count += n-j;
+//                 freq[s[i] -'a']--;
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return count;
+//     }
+// };
 
 
 //                  ********** BRUTE FORCE ***********

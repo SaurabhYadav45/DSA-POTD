f0a0b642c7745e095c11cdbaebf9e07e63853fc4
Saurabh Singh  Yadav <146427013+SaurabhYadav45@users.noreply.github.com>
2025-09-03 13:40:07 +0530

    Time: 443 ms (72.59%) | Memory: 154.6 MB (67.92%) - LeetSync

---
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        // Find cumulative sum for each row
+        for(int row=0; row<rows; row++){
+            for(int col=1; col<cols; col++){
+                matrix[row][col] += matrix[row][col-1];
+            }
+        }
+        // Now apply the approach: subarray sum equals to k in 1D array
+        int result = 0;
+
+        for(int startCol = 0; startCol<cols; startCol++){
+            for(int currCol=startCol; currCol<cols; currCol++){
+                unordered_map<int, int>mp;
+                mp[0] = 1;
+                int cumSum = 0;
+                for(int row=0; row<rows; row++){
+                    cumSum = cumSum + matrix[row][currCol] - (startCol > 0 ? matrix[row][startCol-1] :0);
+                    if(mp.find(cumSum-target) != mp.end()){
+                        result += mp[cumSum-target];
+                    }
+                    mp[cumSum]++;
+                }
+            }
+        }
+        return result;
+    }
+};
